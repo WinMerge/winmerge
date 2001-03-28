aeb7d487d97db5d9707060a6d5335c9ae5b5a1cc
grimmd <none@none>
2001-03-28 04:08:47 +0000

    removed CSplitterWndEx reference

--- Src/ChildFrm.h
+++ Src/ChildFrm.h
@@ -28,7 +28,7 @@
 #pragma once
 #endif // _MSC_VER >= 1000
 
-#include "SplitterWndEx.h"
+//#include "SplitterWndEx.h"
 
 class CChildFrame : public CMDIChildWnd
 {
@@ -39,7 +39,7 @@ public:
 
 // Attributes
 protected:
-	CSplitterWndEx m_wndSplitter;
+	CSplitterWnd m_wndSplitter;
 public:
 
 // Operations
