89e9d185adc10eba6e140953467acc4909ad2bcf
Dennis Lim <dlkj@users.sourceforge.net>
2001-06-19 14:54:42 +0000

    Add Redo feature

--- Src/resource.h
+++ Src/resource.h
@@ -251,7 +251,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_3D_CONTROLS                     1
 #define _APS_NEXT_RESOURCE_VALUE        135
-#define _APS_NEXT_COMMAND_VALUE         32806
+#define _APS_NEXT_COMMAND_VALUE         32807
 #define _APS_NEXT_CONTROL_VALUE         1023
 #define _APS_NEXT_SYMED_VALUE           106
 #endif
