3207627d6cf57c1665bf22bfe34fe9c5e88cc468
Perry Rapp <elsapo@users.sourceforge.net>
2003-07-21 03:31:14 +0000

    Added ChineseTraditional, ChineseSimplified, Korean translations.

--- Src/Languages/BuildAll.bat
+++ Src/Languages/BuildAll.bat
@@ -1,7 +1,11 @@
+Call BuildDll ChineseSimplified
+Call BuildDll ChineseTraditional
 Call BuildDll Czech
 Call BuildDll Danish
 Call BuildDll Dutch
 Call BuildDll French
 Call BuildDll German
 Call BuildDll Italian
+Call BuildDll Korean
+Call BuildDll Slovak
 Call BuildDll Spanish
