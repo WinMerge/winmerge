13ed112ce313ae85790b665eecacc186b179e1b7
Kimmo Varis <kimmov@gmail.com>
2003-07-25 10:33:34 +0000

    Oops. Fix comment format.

--- ShellExtension/Register.bat
+++ ShellExtension/Register.bat
@@ -1,12 +1,13 @@
-; Register.bat
-;
-; ShellExtension.dll is a so called 'Shell Extension'
-; allowing user to start WinMerge from explorer's context
-; menu. However this dll file must be registered for
-; Windows before it works.
-;
-; ShellExtension.dll can be unregistered with command:
-; regsvr32 -u Shell_Extension.dll
+# Register.bat
+#
+# ShellExtension.dll is a so called 'Shell Extension'
+# allowing user to start WinMerge from explorer's context
+# menu to compare selected files/directories.
+# However this dll file must be registered for
+# Windows before it works.
+#
+# ShellExtension.dll can be unregistered with command:
+# regsvr32 -u Shell_Extension.dll
 
 echo Registering ShellExtension.dll...
 regsvr32 ShellExtension.dll
