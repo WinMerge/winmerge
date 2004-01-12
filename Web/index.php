8c0bd97c29d6225de48768fade4d9f3626042d4a
Christian List <list1974@hotmail.com>
2004-01-12 18:00:29 +0000

    Exact copy of what is currently on our website

---
+++ Web/index.php
@@ -0,0 +1,237 @@
+<html>
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<meta name="DESCRIPTION" content="WinMerge HomePage: Visual text file comparison and merging (Open Source software).">
+<meta name="KEYWORDS" content="WinMerge, free,download,Beyond Compare, Scooter Software, DLSuperC, DLSubCF, Component Software Diff, Viff, TreeDiff, FileSync, WMatch, Quick Spot, NTDMatch, windiff, WDIFF, FolderMatch, Merge 99, Merge99, PMdiff, PMDiff, PMDIFF, pmdiff, visual, visually, file, folder, directory, synchronization, comparison, graphical, document, text, compare, diff, difference, revision, Araxis, compare folders, compare files, synchronize folders, merge files, merge folders, Y2K, Year 2000, Year 2K, side-by-side, VFC, GFC, gdiff, CSDiff, OS2GFC, vdiff, ediff, fc">
+<title>Welcome to WinMerge on SourceForge!</title>
+</head>
+
+<body>
+
+<table border="0" cellpadding="4">
+  <tr>
+    <td width="200" valign="top" bgcolor="#c0c0c0" bordercolor="#000000">
+      <p align="center"><b><i><FONT face=Arial>WinMerge on</FONT> </i></b><br><a href="http://sourceforge.net/projects/winmerge"><FONT 
+      face=Arial><IMG src="http://sourceforge.net/sflogo.php?group_id=13216" border="0" alt="SourceForge Logo"></FONT></a></p>
+      <p align="center"><b><FONT 
+      face=Arial>Users</FONT></b></p>
+      <p align="left"><font size="2" ><A href="docs20/index.html"><FONT face=Arial>WinMerge User's 
+      Guide</FONT>
+       </a><br>
+      <a href="http://sourceforge.net/mail/?group_id=13216"><FONT face=Arial>Mailing Lists</FONT> </a><br>
+      <a href="http://sourceforge.net/bugs/?group_id=13216"><FONT face=Arial>Submit a Bug Report<br></FONT>
+      </a><A href="WinMergeChanges.html"><FONT 
+      face=Arial>Change History</FONT> </a></font></p>
+      <p align="center"><b><FONT 
+      face=Arial>Developers</FONT></b></p>
+      <p align="left"><font size="2" ><a href="http://sourceforge.net/projects/winmerge/"><FONT 
+      face=Arial>Project Home</FONT>
+      </a><FONT face=Arial>&nbsp;</FONT><a href="http://cvs.sourceforge.net/cgi-bin/viewcvs.cgi/winmerge"><br><FONT face=Arial>Go to 
+      CVSWeb</FONT>
+        </a><br>
+      <a href="http://sourceforge.net/cvs/?group_id=13216"><FONT face=Arial>CVS Client Access<br></FONT>
+      </a><A href="#UsingWinCVS"><FONT face=Arial>WinCVS Hints</FONT> </a><br>
+      <a href="http://sourceforge.net/patch/?group_id=13216"><FONT face=Arial>Submit a Patch<br></FONT>
+      </a><A href="#translate"><FONT face=Arial>Translate 
+      WinMerge!</FONT> </a><b><br>
+      </b></font></p>
+      <p align="center"><b><FONT 
+      face=Arial>Downloads</FONT></b></p>
+      <p align="left"><font size="2" ><a href="http://prdownloads.sourceforge.net/winmerge/WinMergeSetup202.exe"><FONT face=Arial>WinMerge 2.0.2 Installer</FONT>
+      </a><br>
+      <a href="http://prdownloads.sourceforge.net/winmerge/WinMergeDocs2_0.zip"><FONT face=Arial>Documentation</FONT></a></font></p>
+      <p align="left"><font size="2" face=Ariel>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeBrazilian20.zip"><FONT face=Arial>Brazilian Portuguese DLL</FONT><br></a>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeCatalan20.zip"><FONT face=Arial>Catalan DLL</FONT><br></a>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeChineseSimplifiedGB2312_200RC2.zip"><FONT face=Arial>Chinese Simplified GB2313 DLL</FONT><br></a>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeChineseTraditionalBIG5_200RC2.zip"><FONT face=Arial>Chinese Traditional BIG5 DLL</FONT><br></a>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeCzech_200RC2.zip"><FONT face=Arial>Czech DLL</FONT><br></a>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeDanish_200RC2.zip"><FONT face=Arial>Danish DLL</FONT><br></a>
+	<a href="http://prdownloads.sourceforge.net/winmerge/MergeDutch_200RC2.zip"><FONT face=Arial>Dutch DLL</FONT>
+      </a><br>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeFrench_200RC2.zip"><FONT face=Arial>French DLL</FONT>
+      </a><br>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeGerman_200RC2.zip"><FONT face=Arial>German DLL</FONT>
+      </a><br>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeItalian_200RC2.zip"><FONT face=Arial>Italian DLL</FONT>
+      </a><br>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeKorean_200RC2.zip"><FONT face=Arial>Korean DLL</FONT><br></a>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeNorwegian20.zip"><FONT face=Arial>Norwegian DLL</FONT><br></a>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeSlovak_200RC2.zip"><FONT face=Arial>Slovak DLL</FONT><br></a>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeSpanish_200RC2.zip"><FONT face=Arial>Spanish DLL</FONT></a><br>
+      <a href="http://prdownloads.sourceforge.net/winmerge/MergeSwedish20.zip"><FONT face=Arial>Swedish DLL</FONT></a><br>
+      </font></p>
+      <p align="left"><FONT face=Arial></FONT>&nbsp;</p></td>
+    <td width=*><h1><IMG height=32 src="images/winmerge.gif" width =32
+    align=textTop  nosave> WinMerge</h1>
+    <p>An Open Source visual text file differencing and merging tool for Win32 platforms. It is highly useful for determing what has changed between project versions, and then merging changes between versions. </p>
+    <h2>Features</h2>
+    <ul>
+      <li>Visual differencing of text files 
+      <li>Syntax highlighting 
+      <li>Diff a single file, or entire directories 
+      <li>File filters in directory diff
+      <li>Merge differences 
+      <li>Directory recursion 
+      <li>Integrated editor (with multi-level undo/redo)
+      <li>Find & Replace
+      <li>Drag & Drop support
+      <li>Option for integration with Windows Explorer
+      <li>Rudimentary Visual SourceSafe and Rational ClearCase integration 
+      <li>Handles DOS, UNIX and MAC text file formats
+      <li>Localizable interface via resource DLL 
+      <li>HTML-based <A href="docs20/index.html">User's Guide</a></li>
+    </ul>
+
+<IMG src="images/screenshot.png">
+
+<h2>WinMerge 2.0.2 - latest stable version</h2>
+<p>WinMerge 2.0.2 is the latest stable version, and it is recommended for most users</p>
+<p><a href="http://prdownloads.sourceforge.net/winmerge/WinMergeSetup202.exe">Download WinMerge 2.0.2 Installer</a></p>
+
+<h2>WinMerge 2.1 - current development version</h2>
+<p>WinMerge 2.1 is the development version, and is only recommended for users that can tolerate possibly losing their files once in a while, because we do not guarantee it to be stable.</p>
+<p>On the other hand, this version has the latest and greatest features in WinMerge.</P>
+<p><a href="http://sourceforge.net/project/showfiles.php?group_id=13216&release_id=199032">Download WinMerge 2.1</a></p>
+
+<h2>WinMerge 1.7 - older stable version</h2>
+<p><a href="WinMerge17/index.html">WinMerge 1.7</a> is an older and simpler version, from before the integrated editor was introduced.</p>
+<p><a href="http://prdownloads.sourceforge.net/winmerge/WinMergeSetup.exe">Download WinMerge 1.7 Installer</a></p>
+
+
+    <h2>Installation</h2>
+    <p>Just download the installer and double-click it.</p>
+
+    <h2>Using WinMerge</h2>
+    <p>We actually dug up some time and created a simple User's Guide. <A
+    href="docs20/index.html">Check it out</a>.</p>
+
+    <h2>Support</h2>
+      <p>WinMerge is now an Open Source project, that is covered
+    under the <a href="http://www.gnu.org/copyleft/gpl.html">GNU Public License</a>.&nbsp;
+      As such, your main form of support for WinMerge will be through
+      subscription to the <a href="http://lists.sourceforge.net/lists/listinfo/winmerge-support">WinMerge-support mailing list</a>.
+      In addition, SourceForge has bug tracking capabilities, so please report any
+    bugs <a href="http://sourceforge.net/bugs/?group_id=13216">here</a>.&nbsp; Wish list items on the 
+    <a href="http://sourceforge.net/tracker/?group_id=13216&atid=363216">RFE list</a> will also be considered, but we make
+    absolutely no promises.&nbsp; </p>
+    <p>Here is the current <A href="WinMergeChanges.html">change history</a>. </p>
+
+    <h2><a name="translate"></a>Localization</h2>
+    <p>If you are interested in translating WinMerge's resources to your own native language,
+    we'd be happy to provide you with the .RC file. All the resources are moved into a
+    DLL, so it should be fairly straightforward to support most languages. <A
+    href="mailto:winmerge-support@lists.sourceforge.net">Let us know</a> if you're interested. We currently have
+WinMerge translated into the languages listed below. 
+To change languages, all you have to do is unzip the DLL in the same directory as WinMerge.exe, then
+    select View / Language and choose your language. </p>
+
+<h3>Current translations of WinMerge</h3>
+<ul>
+	<li>Brazilian Portuguese translated by Wender Firmino
+	<li>Catalan translated by Jordi Vilar
+	<li>Simplified Chinese translated by Liaobin
+	<li>Traditional Chinese translated by Koko
+	<li>Czech translated by Jiri Tax
+	<li>Danish translated by Christian List
+	<li>Dutch translated by Ronald Schaap
+	<li>French translated by Gil Andre, continued by Laurent Ganier
+	<li>German translated by Winfried Peter, continued by Joerg Schneider
+	<li>Italian translated by Michele Merega
+	<li>Korean translated by Sukjoon
+	<li>Norwegian translated by Hans Fredrik Nordhaug
+	<li>Slovak translated by majvan
+	<li>Spanish translated by Dean Grimm
+	<li>Swedish translated by Göran Håkansson 
+</ul>
+    <h2>Developers Needed!</h2>
+      <p>One of the reasons WinMerge is on SourceForge is so other
+      developers can get involved.&nbsp; If you have some free time, are
+      proficient with DeveloperStudio 6 and MFC, and would like to contribute,
+      then <a href="http://sourceforge.net/tracker/?group_id=13216&atid=113216">check
+      out the list of items to be done</a>.&nbsp; If you see something in there that you think
+      you can handle, <a href="mailto:winmerge-support@lists.sourceforge.net">let us know</a> and we'll assign the item to you.&nbsp; Then
+      grab a copy of the WinMerge source from <a href="http://sourceforge.net/cvs/?group_id=13216">anonymous
+      CVS</a> or you can download a <a href="http://sourceforge.net/project/showfiles.php?group_id=13216">zipped version</a> of the source,
+	write your code, and then upload the changed code <a href="http://sourceforge.net/tracker/?group_id=13216&atid=313216">as a patch</a>. 
+	Then we will merge them back into the main codebase. </p>
+      <p>If you do decide to join in, please follow these simple guidelines,
+      which will ultimately make all our lives easier: </p>
+      <ul>
+        <li>Please consider that WinMerge is translated into 
+        multiple languages, so it is always better to keep UI changes to a 
+        minimum.&nbsp; Any addition of text to the UI means we have to go back 
+        to the translators. 
+        <li><b>Do not </b>make trivial 
+        changes to existing code, such as deleting blanks, consolidating 
+        multi-line comments into single line comments, etc.&nbsp; This just adds 
+        a bunch of needless differences to the code.&nbsp; Besides, if every 
+        developer massaged the code to fit his/her style, it would be a 
+        never-ending struggle 
+        <li>Please try to match existing coding style as 
+        much as possible 
+        <li>Please make sure to comment your work so we can 
+        all understand what you've done (even though I didn't ;-) ) 
+        <li>Please don't make changes that don't relate to 
+        the task at hand.&nbsp; If you come across other issues while handling a 
+        task, please submit them as bugs or patches 
+        <li>Please don't make changes that aren't on the task list or bug list;
+          if everyone just threw in everything they wanted, the product would
+          become a hodge-podge</li>
+      </ul>
+      <p>The general plan is to start people with anonymous CVS access and let the regular developers commit
+      their changes to the CVS repository. Those developers that
+      continue to contribute will be considered for read-write CVS access.<br>
+      </p>
+
+    <h2>WinMerge developers</h2>
+	<p>This is a list of some of the current developers on WinMerge</p>
+	<ul>
+  		<li>Chris Mumford
+  		<li>Christian List
+  		<li>Dean Grimm
+  		<li>Dennis Limm
+  		<li>Kimmo Varis
+  		<li>Laurent Ganier
+  		<li>Perry Rapp
+	</ul>
+
+    <h2><a name="UsingWinCVS"></a>Using WinCVS</h2>
+      <p>After much trial and error, I finally seem to have hit the sweet spot
+      with WinCVS, and it works pretty well.&nbsp; To get access to the
+      anonymous CVS server, do the following (note that this does not require
+      SSH). </p>
+      <ol>
+        <li>Install <a href="http://www.wincvs.org">WinCVS 1.2</a>
+        <li>Create a directory on your system that will 
+        contain the WinMerge and Common repositories (I call it Merge on my 
+        system) 
+        <li>In WinCVS, select "Change Browse Location" from 
+        the View menu and navigate to the directory you just created.&nbsp; Your 
+        base folder should now appear in the left pane of the WinCVS main window 
+        and should be selected. 
+        <li>Select "Checkout module" from the Create menu 
+        <li>Enter "WinMerge" (no quotes) in the "Enter 
+        module name and path on the server" field.&nbsp; Note that 
+        capitalization is important. 
+        <li>Select the General tab 
+        <li>Enter 
+        ":pserver:anonymous@cvs.WinMerge.sourceforge.net:/cvsroot/winmerge" in 
+        the "Enter the CVSROOT" field (again, no quotes) 
+        <li>Select "passwd file on the CVS server" in the 
+        Authentication section 
+        <li>Click OK.&nbsp; This should download a copy of 
+        the source code into a subdirectory called WinMerge. 
+        <li>Repeat steps 4-9, for these modules: "common", "Docs", "Filters", 
+	"InnoSetup", "Languages", "MakeResDll", "Plugins" and "ShellExtension". 
+	Check out the modules into the same base folder. </li>
+      </ol>
+    <p> </p>
+      <p>&nbsp;</p></td>
+  </tr>
+</table>
+
+</body>
+
+</html>
