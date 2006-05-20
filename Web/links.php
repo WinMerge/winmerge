da6714d3fd3d06ad26c1f706b940575ef73be153
Tim Gerundt <tim@gerundt.de>
2006-05-20 09:23:48 +0000

    RFE: [ 1224436 ] WinMerge as an Eclipse IDE plugin?

--- Web/links.php
+++ Web/links.php
@@ -13,6 +13,8 @@
   <dd><b>WinSCP</b> is an open source <acronym title="SSH File Transfer Protocol">SFTP</acronym> and <acronym title="Secure Copy Protocol">SCP</acronym> client for Windows using <acronym title="Secure Shell">SSH</acronym>.</dd>
   <dt><a href="http://gnuwin32.sourceforge.net/">GnuWin32</a></dt>
   <dd><b>GnuWin32</b> provides Win32 ports of tools with a <a href="http://www.gnu.org/">GNU</a> or similar <a href="http://opensource.org/licenses/">open source license</a>.</dd>
+  <dt><a href="https://sourceforge.net/projects/externaldiff">External diff Tool</a></dt>
+  <dd><b>External diff Tool</b> is an <a href="http://www.eclipse.org/">Eclipse</a> Plugin</a> that allows the launch of WinMerge (or an other external diff tool) for file compares rather than the default built-in Eclipse diff tool.</dd>
 </dl>
 <h3>CVS</h3>
 <dl>
