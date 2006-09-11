ab7921f88a092a717a0a0fb243c7a227068861f8
Tim Gerundt <tim@gerundt.de>
2006-09-11 21:23:26 +0000

    RFE: [ 1552813 ] Web: Mark developers active/inactive

--- Web/developer/index.php
+++ Web/developer/index.php
@@ -9,32 +9,44 @@
 <?php
   $xmlarray = $page->convertXml2Array('developers.xml');
   
-  print("<ul>\n");
+  $activelist = ''; $inactivelist = '';
   $developers = $xmlarray['DEVELOPERS'][0]['DEVELOPER'];
   $developers = $page->multisortArray($developers, 'NAME', SORT_ASC);
   for ($i = 0; $i < count($developers); $i++) { //For all developers...
     $developer = $developers[$i];
+    $developer_inactive = $developer['ATTRIBUTES']['INACTIVE'];
     $developer_name = $developer['NAME'][0]['VALUE'];
     $developer_username = $developer['USERNAME'][0]['VALUE'];
     $developer_role = $developer['ROLE'][0]['VALUE'];
     $developer_email = $developer['EMAIL'][0]['VALUE'];
     
+    $li = '';
     switch ($developer_role) {
       case 1: //Project Manager
-        print("  <li><strong><a href=\"mailto:" . $developer_email . "\" title=\"Project Manager: " . $developer_name . " &lt;" . $developer_email . "&gt;\">" . $developer_name . "</a></strong></li>");
+        $li = "  <li><strong><a href=\"mailto:" . $developer_email . "\" title=\"Project Manager: " . $developer_name . " &lt;" . $developer_email . "&gt;\">" . $developer_name . "</a></strong></li>";
         break;
       case 2: //Developer
-        print("  <li><a href=\"mailto:" . $developer_email . "\" title=\"Developer: " . $developer_name . " &lt;" . $developer_email . "&gt;\">" . $developer_name . "</a></li>");
+        $li = "  <li><a href=\"mailto:" . $developer_email . "\" title=\"Developer: " . $developer_name . " &lt;" . $developer_email . "&gt;\">" . $developer_name . "</a></li>";
         break;
       case 3: //Packager
-        print("  <li><a href=\"mailto:" . $developer_email . "\" title=\"Packager: " . $developer_name . " &lt;" . $developer_email . "&gt;\">" . $developer_name . "</a></li>");
+        $li = "  <li><a href=\"mailto:" . $developer_email . "\" title=\"Packager: " . $developer_name . " &lt;" . $developer_email . "&gt;\">" . $developer_name . "</a></li>";
         break;
       case 4: //Web Designer
-        print("  <li><a href=\"mailto:" . $developer_email . "\" title=\"Web Designer: " . $developer_name . " &lt;" . $developer_email . "&gt;\">" . $developer_name . "</a></li>");
+        $li = "  <li><a href=\"mailto:" . $developer_email . "\" title=\"Web Designer: " . $developer_name . " &lt;" . $developer_email . "&gt;\">" . $developer_name . "</a></li>";
         break;
     }
+    if ($developer_inactive != '1') $activelist .= $li; else $inactivelist .= $li;
   }
+  print("<ul>\n");
+  print($activelist);
   print("</ul>\n");
+  
+  if ($inactivelist != '') {
+    print("<h3>Inactive/past developers:</h3>\n");
+    print("<ul>\n");
+    print($inactivelist);
+    print("</ul>\n");
+  }
 ?>
 <p>Look here for more information on how to <a href="needed.php">develop</a> and/or 
 <a href="l10n/index.php">translate</a> WinMerge.</p>
