94b71d11410f96f7b84a1c76732c7c6479b6eddc
Tim Gerundt <tim@gerundt.de>
2007-01-18 22:52:23 +0000

    PATCH: [ 1638998 ] Web: Changes for new stable release 2.6.4

--- Web/2.6/index.php
+++ Web/2.6/index.php
@@ -30,8 +30,8 @@
 <p>See the <a href="screenshots.php">screenshots page</a> for more screenshots.</p>
 
 <h2>Installation</h2>
-<p>Just download the <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-Setup.exe">installer</a> and double-click it.</p>
-<p>Or you may simply unzip the <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-exe.zip">binary zip package</a> into the desired location, and run
+<p>Just download the <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-Setup.exe">installer</a> and double-click it.</p>
+<p>Or you may simply unzip the <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-exe.zip">binary zip package</a> into the desired location, and run
 <code>WinMergeU.exe</code> (except for Windows 98/ME users who should use <code>WinMerge.exe</code>).</p>
 <p>For more information, please see the <a href="manual/Installing.html">Installation section</a> of the <a href="manual/index.html">Manual</a>.</p>
 
--- Web/downloads.php
+++ Web/downloads.php
@@ -5,14 +5,14 @@
   $page->printHead('WinMerge: Downloads', WINMERGE_USR);
 ?>
 <h2>Downloads</h2>
-<h3><a href="http://sourceforge.net/project/showfiles.php?group_id=13216&amp;package_id=11248&amp;release_id=471214">WinMerge 2.6.2</a></h3>
+<h3><a href="http://sourceforge.net/project/showfiles.php?group_id=13216&amp;package_id=11248&amp;release_id=479250">WinMerge 2.6.4</a></h3>
 <dl>
-  <dt><a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-Setup.exe">Installer</a></dt>
+  <dt><a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-Setup.exe">Installer</a></dt>
   <dd><p>The easiest way to install WinMerge is to download and run this installer. Read the <a href="2.6/manual/Installing.html">online manual</a> for help using it.</p></dd>
-  <dt><a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-exe.zip">Binaries</a></dt>
-  <dd><p>WinMerge binaries are also available in archive files (<a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-exe.zip">.zip</a> and <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-exe.7z">.7z</a>).</p></dd>
-  <dt><a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-src.zip">Source code</a></dt>
-  <dd><p>Source code is available in archive files (<a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-src.zip">.zip</a> and <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.2-src.7z">.7z</a>).</p></dd>
+  <dt><a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-exe.zip">Binaries</a></dt>
+  <dd><p>WinMerge binaries are also available in archive files (<a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-exe.zip">.zip</a> and <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-exe.7z">.7z</a>).</p></dd>
+  <dt><a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-src.zip">Source code</a></dt>
+  <dd><p>Source code is available in archive files (<a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-src.zip">.zip</a> and <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.6.4-src.7z">.7z</a>).</p></dd>
   <dt><a href="http://prdownloads.sourceforge.net/winmerge/Merge7zInstaller0018-311-442.exe ">7-Zip Plugin</a> (DllBuild 0018)</dt>
   <dd>
     <p>This is the 7-Zip plugin for WinMerge.</p>
