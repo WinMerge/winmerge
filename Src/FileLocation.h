db74437c652735b5ceb8ef818080c8d21d365828
Kimmo Varis <kimmov@gmail.com>
2008-02-19 19:52:54 +0000

    Add doxygen comments for FileLocation struct.

--- Src/FileLocation.h
+++ Src/FileLocation.h
@@ -1,3 +1,12 @@
+/** 
+ * @file FileLocation.h
+ *
+ * @brief Declaration for FileLocation class.
+ *
+ */
+// ID line follows -- this is updated by SVN
+// $Id$
+
 #ifndef FileLocation_included
 #define FileLocation_included
 
@@ -5,17 +14,35 @@
 #include "FileTextEncoding.h"
 #endif
 
+/**
+ * @brief A structure containing file's path and encoding information.
+ */
 struct FileLocation
 {
-	CString filepath;
-	FileTextEncoding encoding;
+	CString filepath; /**< Full path for the file. */
+	FileTextEncoding encoding; /**< Encoding info for the file. */
+
+	/**
+	 * The default constructor.
+	 */
 	FileLocation()
 	{
 	}
+
+	/**
+	 * The constructor taking a path as a parameter.
+	 * @param [in] path Full path for the file.
+	 */
 	FileLocation(LPCTSTR path) : filepath(path)
 	{
 	}
+
 // Methods
+
+	/**
+	 * Set the file path.
+	 * @param [in] sFilePath Full path for the file.
+	 */
 	void setPath(const CString & sFilePath) { this->filepath = sFilePath; }
 
 // Copy constructor & copy operator should do the obvious correct things
