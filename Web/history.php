26d26d92b7a9520c53fa16fe43d43cda90e07513
Tim Gerundt <tim@gerundt.de>
2008-02-26 22:55:51 +0000

    Web: Add a first version from a WinMerge history list

---
+++ Web/history.php
@@ -0,0 +1,43 @@
+<?php
+  include('page.inc');
+
+  $page = new Page;
+  $page->printHead('WinMerge: History', TAB_HOME);
+?>
+<h2>History</h2>
+<h3>WinMerge 2.6</h3>
+<ul>
+  <li>WinMerge 2.6.14 (2008-01-17)</li>
+  <li>WinMerge 2.6.12 (2007-09-26)</li>
+  <li>WinMerge 2.6.8 (2007-06-19)</li>
+  <li>WinMerge 2.6.6 (2007-03-29)</li>
+  <li>WinMerge 2.6.4 (2007-01-17)</li>
+  <li>WinMerge 2.6.2 (2006-12-14)</li>
+  <li>WinMerge 2.6.0 (2006-10-12)</li>
+</ul>
+<h3>WinMerge 2.4</h3>
+<ul>
+  <li>WinMerge 2.4.10 (2006-08-10)</li>
+  <li>WinMerge 2.4.8 (2006-06-13)</li>
+  <li>WinMerge 2.4.6 (2006-02-28)</li>
+  <li>WinMerge 2.4.4 (2005-12-15)</li>
+  <li>WinMerge 2.4.2 (2005-11-10)</li>
+  <li>WinMerge 2.4.0 (2005-09-29)</li>
+</ul>
+<h3>WinMerge 2.2</h3>
+<ul>
+  <li>WinMerge 2.2.4 (2005-05-31)</li>
+  <li>WinMerge 2.2.2 (2005-03-23)</li>
+  <li>WinMerge 2.2.0 (2004-11-10)</li>
+</ul>
+<h3>WinMerge 2.0</h3>
+<ul>
+  <li>WinMerge 2.0.2 (2003-12-10)</li>
+</ul>
+<h3>WinMerge 1.7</h3>
+<ul>
+  <li>WinMerge 1.7.1 (2000-11-03)</li>
+</ul>
+<?php
+  $page->printFoot();
+?>
