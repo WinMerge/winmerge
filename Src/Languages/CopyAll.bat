4915a4be88e7adcc4c75da81c54574c3b9fe2aa5
Kimmo Varis <kimmov@gmail.com>
2008-02-28 17:58:12 +0000

    Remove unneeded CopyAll.bat. It was used to copy lang files.

--- Src/Languages/CopyAll.bat
+++
@@ -1,4 +0,0 @@
-Copy DLL\*.lang ..\..\Build\MergeDebug\Languages
-Copy DLL\*.lang ..\..\Build\MergeRelease\Languages
-Copy DLL\*.lang ..\..\Build\MergeUnicodeDebug\Languages
-Copy DLL\*.lang ..\..\Build\MergeUnicodeRelease\Languages
