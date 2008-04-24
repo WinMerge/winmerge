675f93700ed1f2e0a10b234a27bef65ec6ed41c4
Tim Gerundt <tim@gerundt.de>
2008-04-24 20:53:09 +0000

    BUG: [ 1838320 ] Links to download page is wrong

--- Web/2.0/index.php
+++ Web/2.0/index.php
@@ -27,13 +27,13 @@
 <img src="images/screenshot.png" alt="Screenshot of WinMerge">
 
 <h2>Installation</h2>
-<p>Just download the <a href="http://prdownloads.sourceforge.net/winmerge/WinMergeSetup202.exe">installer</a> and double-click it.</p>
-<p>Or you may simply unzip the <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge202-exe.zip">binary zip package</a> into the desired location, and run
+<p>Just download the <a href="http://downloads.sourceforge.net/winmerge/WinMergeSetup202.exe">installer</a> and double-click it.</p>
+<p>Or you may simply unzip the <a href="http://downloads.sourceforge.net/winmerge/WinMerge202-exe.zip">binary zip package</a> into the desired location, and run
 <code>WinMergeU.exe</code> (except for Windows 98/ME users who should use <code>WinMerge.exe</code>).</p>
 
 <h2>Using WinMerge</h2>
 <p>You can use the <a href="docs/index.html">online User's Guide</a>, or you can
-<a href="http://prdownloads.sourceforge.net/winmerge/WinMergeDocs2_0.zip">download</a> a copy.</p>
+<a href="http://downloads.sourceforge.net/winmerge/WinMergeDocs2_0.zip">download</a> a copy.</p>
 
 <p>Here is the current <a href="changes.php">change history</a>.</p>
 <?php
--- Web/2.2/index.php
+++ Web/2.2/index.php
@@ -29,8 +29,8 @@
 <p>See the <a href="screenshots.php">screenshots page</a> for more screenshots.</p>
 
 <h2>Installation</h2>
-<p>Just download the <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.2.4-Setup.exe">installer</a> and double-click it.</p>
-<p>Or you may simply unzip the <a href="http://prdownloads.sourceforge.net/winmerge/WinMerge-2.2.4-exe.zip">binary zip package</a> into the desired location, and run
+<p>Just download the <a href="http://downloads.sourceforge.net/winmerge/WinMerge-2.2.4-Setup.exe">installer</a> and double-click it.</p>
+<p>Or you may simply unzip the <a href="http://downloads.sourceforge.net/winmerge/WinMerge-2.2.4-exe.zip">binary zip package</a> into the desired location, and run
 <code>WinMergeU.exe</code> (except for Windows 98/ME users who should use <code>WinMerge.exe</code>).</p>
 <p>For more details please look at the <a href="manual/installing.html">Installation section</a> from the <a href="manual/index.html">Manual</a>.</p>
 
