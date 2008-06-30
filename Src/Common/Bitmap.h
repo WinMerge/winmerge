02c45dfde040cd72ada88182c25162ea6f4ce633
Marcel Gosselin <marcelgosselin@users.sourceforge.net>
2008-06-30 01:16:38 +0000

    Make header files self-contained

--- Src/Common/Bitmap.h
+++ Src/Common/Bitmap.h
@@ -10,6 +10,10 @@
 #ifndef _BITMAP_H_
 #define _BITMAP_H_
 
+class CBitmap;
+class CDC;
+class CRect;
+
 CBitmap *CopyRectToBitmap(CDC *pDC, const CRect & rect);
 void DrawBitmap(CDC *pDC, int x, int y, CBitmap *pBitmap);
 CBitmap *GetDarkenedBitmap(CDC *pDC, CBitmap *pBitmap);
--- Src/Common/StatLink.h
+++ Src/Common/StatLink.h
@@ -9,6 +9,8 @@
 // to any file on your desktop or web. You can use it in dialog boxes
 // to create hyperlinks to web sites. When clicked, opens the file/URL
 //
+#include <afxwin.h>
+
 class CStaticLink : public CStatic {
 public:
 	CStaticLink();
