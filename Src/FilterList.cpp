a59e86df548228bf309a58c913bedf7c857eb578
Kimmo Varis <kimmov@gmail.com>
2008-07-03 19:05:36 +0000

    Add DEBUG_NEW define.

--- Src/FilterList.cpp
+++ Src/FilterList.cpp
@@ -12,6 +12,12 @@
 #include "pcre.h"
 #include "unicoder.h"
 
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#undef THIS_FILE
+static char THIS_FILE[] = __FILE__;
+#endif
+
 /** 
  * @brief Constructor.
  */
