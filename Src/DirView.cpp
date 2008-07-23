cdfb3e78eff95a7c13031487a59f34c891e469dd
Kimmo Varis <kimmov@gmail.com>
2008-07-23 17:35:05 +0000

    BUG: [ 2023734 ] Errors Moving Files - v.2.4.8.0 Unicode  Disable moving protected items.

--- Src/DirView.cpp
+++ Src/DirView.cpp
@@ -3044,7 +3044,14 @@ void CDirView::OnCtxtDirMoveRightTo()
  */
 void CDirView::OnUpdateCtxtDirMoveRightTo(CCmdUI* pCmdUI) 
 {
-	DoUpdateCtxtDirMoveRightTo(pCmdUI);
+	// Because move deletes original item, origin side
+	// cannot be read-only
+	if (GetDocument()->GetReadOnly(FALSE))
+		pCmdUI->Enable(FALSE);
+	else
+	{
+		DoUpdateCtxtDirMoveRightTo(pCmdUI);
+	}
 }
 
 /**
@@ -3052,7 +3059,14 @@ void CDirView::OnUpdateCtxtDirMoveRightTo(CCmdUI* pCmdUI)
  */
 void CDirView::OnUpdateCtxtDirMoveLeftTo(CCmdUI* pCmdUI) 
 {
-	DoUpdateCtxtDirMoveLeftTo(pCmdUI);
+	// Because move deletes original item, origin side
+	// cannot be read-only
+	if (GetDocument()->GetReadOnly(TRUE))
+		pCmdUI->Enable(FALSE);
+	else
+	{
+		DoUpdateCtxtDirMoveLeftTo(pCmdUI);
+	}
 }
 
 /**
