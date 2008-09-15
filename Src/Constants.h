28996b350d316eaa47712f24a576b621bb6e0c7f
Kimmo Varis <kimmov@gmail.com>
2008-09-15 19:26:07 +0000

    Change Manual url to http://winmerge.org/docs/manual/
    --HG--
    branch : R2_10

--- Src/Constants.h
+++ Src/Constants.h
@@ -17,7 +17,7 @@ const TCHAR WinMergeURL[] = _T("http://winmerge.org/");
  * @brief URL to help index in internet.
  * We use internet help when local help file is not found (not installed).
  */
-const TCHAR DocsURL[] = _T("http://winmerge.org/2.8/manual/index.html");
+const TCHAR DocsURL[] = _T("http://winmerge.org/docs/manual/index.html");
 
 /** @brief URL to translations page in internet. */
 const TCHAR TranslationsUrl[] = _T("http://winmerge.org/translations/");
