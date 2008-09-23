ea9c099ad1c95190958d54145ba7672e488f21b4
Tim Gerundt <tim@gerundt.de>
2008-09-23 22:12:35 +0000

    PATCH: [ 2118516 ] Web: Move Wiki link to "Docs" tab

--- Web/docs/index.php
+++ Web/docs/index.php
@@ -11,6 +11,8 @@
 <p>The <a href="releasenotes.php">release notes</a> are a short summary of important changes, enhancements, bug fixes and <a href="releasenotes.php#issues">known issues</a> in the current WinMerge release.</p>
 <h3><a href="changelog.php">Change Log</a></h3>
 <p>The <a href="changelog.php">change log</a> is a more complete list of changes in the last WinMerge releases.</p>
+<h3><a href="/Wiki/">Developer Wiki</a></h3>
+<p>The <a href="/Wiki/">Developer Wiki</a> contains many informations about the WinMerge development.</p>
 <?php
   $page->printFoot();
 ?>
