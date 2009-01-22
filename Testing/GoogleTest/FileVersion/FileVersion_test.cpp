19e2b1208b10843370eb7b05b2e0e3771a7468ab
Kimmo Varis <kimmov@gmail.com>
2009-01-22 21:59:11 +0000

    Tests: Add version set/get tests for FileVersion class. These tests already found bug that product version was never set.

--- Testing/GoogleTest/FileVersion/FileVersion_test.cpp
+++ Testing/GoogleTest/FileVersion/FileVersion_test.cpp
@@ -46,6 +46,61 @@ namespace
 		FileVersion version;
 	}
 
+	TEST_F(FileVersionTest, getfilever_notset)
+	{
+		FileVersion version;
+		EXPECT_EQ("", version.GetFileVersionString());
+	}
+	TEST_F(FileVersionTest, getprodver_notset)
+	{
+		FileVersion version;
+		EXPECT_EQ("0.0.0.0", version.GetProductVersionString());
+	}
+
+	TEST_F(FileVersionTest, getfilever_zeros)
+	{
+		FileVersion version;
+		version.SetFileVersion(0, 0);
+		EXPECT_EQ("0.0.0.0", version.GetFileVersionString());
+	}
+
+	TEST_F(FileVersionTest, getfilever_simple1)
+	{
+		FileVersion version;
+		version.SetFileVersion(1, 2);
+		EXPECT_EQ("0.1.0.2", version.GetFileVersionString());
+	}
+	TEST_F(FileVersionTest, getfilever_real)
+	{
+		FileVersion version;
+		DWORD hi = (1 << 16) | 2;
+		DWORD lo = (3 << 16) | 4;
+		version.SetFileVersion(hi, lo);
+		EXPECT_EQ("1.2.3.4", version.GetFileVersionString());
+	}
+	
+	TEST_F(FileVersionTest, getprodver_zeros)
+	{
+		FileVersion version;
+		version.SetProductVersion(0, 0);
+		EXPECT_EQ("0.0.0.0", version.GetProductVersionString());
+	}
+
+	TEST_F(FileVersionTest, getprodver_simple1)
+	{
+		FileVersion version;
+		version.SetProductVersion(1, 2);
+		EXPECT_EQ("0.1.0.2", version.GetProductVersionString());
+	}
+	TEST_F(FileVersionTest, getprodver_real)
+	{
+		FileVersion version;
+		DWORD hi = (1 << 16) | 2;
+		DWORD lo = (3 << 16) | 4;
+		version.SetProductVersion(hi, lo);
+		EXPECT_EQ("1.2.3.4", version.GetProductVersionString());
+	}
+
 }  // namespace
 
 int main(int argc, char **argv)
