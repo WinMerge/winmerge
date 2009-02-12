18021275dff44873b43c4dd03a33cd5d8a83b673
Kimmo Varis <kimmov@gmail.com>
2009-02-12 18:20:28 +0000

    Tests: Add unit test for UnicodeString. At the moment just one test to add the project.

---
+++ Testing/GoogleTest/UnicodeString/UnicodeString_test.cpp
@@ -0,0 +1,66 @@
+#include <gtest/gtest.h>
+#include <windows.h>
+#include <tchar.h>
+
+#include "UnicodeString.h"
+
+namespace
+{
+	// The fixture for testing paths functions.
+	class UnicodeStringTest : public testing::Test
+	{
+	protected:
+		// You can remove any or all of the following functions if its body
+		// is	empty.
+
+		UnicodeStringTest()
+		{
+			// You can do set-up work for each test	here.
+		}
+
+		virtual ~UnicodeStringTest()
+		{
+			// You can do clean-up work	that doesn't throw exceptions here.
+		}
+
+		// If	the	constructor	and	destructor are not enough for setting up
+		// and cleaning up each test, you can define the following methods:
+
+		virtual void SetUp()
+		{
+			// Code	here will be called	immediately	after the constructor (right
+			// before each test).
+		}
+
+		virtual void TearDown()
+		{
+			// Code	here will be called	immediately	after each test	(right
+			// before the destructor).
+		}
+
+		// Objects declared here can be used by all tests in the test case for Foo.
+	};
+
+	TEST_F(UnicodeStringTest, Construct1)
+	{
+		String str;
+		EXPECT_TRUE(str.empty());
+	}
+
+	TEST_F(UnicodeStringTest, Replace1)
+	{
+		String orig("OneTwoThree");
+		String find("Two");
+		String rep("Five");
+		string_replace(orig, find, rep);
+		EXPECT_EQ("OneFiveThree", orig);
+	}
+
+}  // namespace
+
+int main(int argc, char **argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
