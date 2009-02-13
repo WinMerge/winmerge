7b45efb4c63fe5aa221534dfc1329a7f3fe6d4c6
Kimmo Varis <kimmov@gmail.com>
2009-02-13 18:07:14 +0000

    Tests: Create Google Test project for testing DirItem class.

---
+++ Testing/GoogleTest/DirItem/DirItem_test.cpp
@@ -0,0 +1,57 @@
+#include <gtest/gtest.h>
+#include <windows.h>
+#include <tchar.h>
+#include "UnicodeString.h"
+#include "DirItem.h"
+
+namespace
+{
+	// The fixture for testing paths functions.
+	class DirItemTest : public testing::Test
+	{
+	protected:
+		// You can remove any or all of the following functions if its body
+		// is	empty.
+
+		DirItemTest()
+		{
+			// You can do set-up work for each test	here.
+		}
+
+		virtual ~DirItemTest()
+		{
+			// You can do clean-up work	that doesn't throw exceptions here.
+		}
+
+		// If	the	constructor	and	destructor are not enough for setting up
+		// and cleaning up each test, you can define the following methods:
+
+		virtual void SetUp()
+		{
+			// Code	here will be called	immediately	after the constructor (right
+			// before each test).
+		}
+
+		virtual void TearDown()
+		{
+			// Code	here will be called	immediately	after each test	(right
+			// before the destructor).
+		}
+
+		// Objects declared here can be used by all tests in the test case for Foo.
+	};
+
+	TEST_F(DirItemTest, Construct1)
+	{
+		DirItem item;
+		EXPECT_TRUE(item.ctime == 0);
+	}
+
+
+}  // namespace
+
+int main(int argc, char **argv)
+{
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
