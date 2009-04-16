135b139c1574840c76ed80ced3068ef0f198a7df
Kimmo Varis <kimmov@gmail.com>
2009-04-16 21:21:46 +0000

    Remove Frhed sources from WinMerge source tree. Use stand-alone Frhed and Frhed sources for now on.

--- Externals/heksedit/shtools.h
+++
@@ -1,6 +0,0 @@
-HRESULT NTAPI CreateLink(LPCTSTR lpszPathObj, LPCTSTR lpszPathLink);
-HRESULT NTAPI CreateLink(LPCTSTR lpszPathLink);
-HRESULT NTAPI ResolveIt(HWND hwnd, LPCTSTR lpszLinkFile, LPTSTR lpszPath);
-HRESULT NTAPI PathsEqual(LPCTSTR, LPCTSTR);
-HRESULT NTAPI PathsEqual(LPCTSTR);
-void NTAPI GetLongPathNameWin32(LPCTSTR lpszShortPath, LPTSTR lpszLongPath);
--- Externals/heksedit/toolbar.h
+++
@@ -1,4 +0,0 @@
-#ifndef toolbar_h
-#define toolbar_h
-HWND CreateTBar(HWND, HINSTANCE);
-#endif // toolbar_h
