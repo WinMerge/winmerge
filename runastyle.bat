e93726d06525b645c81ecb5a3c005983cb84f6c0
Kimmo Varis <kimmov@gmail.com>
2009-07-26 14:03:45 +0000

    Tweak Astyle formatting options a bit: - force using tabs - indent continued statements with two tabs - do not break one line code blocks

--- runastyle.bat
+++ runastyle.bat
@@ -4,17 +4,17 @@ REM http://astyle.sourceforge.net/
 REM $Id$
 
 REM Src is Ansi-style - tab-indent
-astyle --style=ansi --indent=tab --pad-oper --unpad-paren Src/*.cpp
-astyle --style=ansi --indent=tab --pad-oper --unpad-paren Src/*.h
+astyle --style=ansi --indent=force-tab --pad-oper --unpad-paren --max-instatement-indent=2 --keep-one-line-blocks Src/*.cpp
+astyle --style=ansi --indent=force-tab --pad-oper --unpad-paren --max-instatement-indent=2 --keep-one-line-blocks Src/*.h
 
-astyle --style=ansi --indent=tab --pad-oper --unpad-paren Src/Common/*.cpp
-astyle --style=ansi --indent=tab --pad-oper --unpad-paren Src/Common/*.h
+astyle --style=ansi --indent=force-tab --pad-oper --unpad-paren --max-instatement-indent=2 --keep-one-line-blocks Src/Common/*.cpp
+astyle --style=ansi --indent=force-tab --pad-oper --unpad-paren --max-instatement-indent=2 --keep-one-line-blocks Src/Common/*.h
 
-astyle --style=ansi --indent=tab --pad-oper --unpad-paren Src/CompareEngines/*.cpp
-astyle --style=ansi --indent=tab --pad-oper --unpad-paren Src/CompareEngines/*.h
+astyle --style=ansi --indent=force-tab --pad-oper --unpad-paren --max-instatement-indent=2 --keep-one-line-blocks Src/CompareEngines/*.cpp
+astyle --style=ansi --indent=force-tab --pad-oper --unpad-paren --max-instatement-indent=2 --keep-one-line-blocks Src/CompareEngines/*.h
 
-astyle --style=ansi --indent=tab --pad-oper --unpad-paren ShellExtension/*.cpp
-astyle --style=ansi --indent=tab --pad-oper --unpad-paren ShellExtension/*.h
+astyle --style=ansi --indent=force-tab --pad-oper --unpad-paren --max-instatement-indent=2 --keep-one-line-blocks ShellExtension/*.cpp
+astyle --style=ansi --indent=force-tab --pad-oper --unpad-paren --max-instatement-indent=2 --keep-one-line-blocks ShellExtension/*.h
 
 REM Editor code is GNU style - 2 space indent
 astyle --style=gnu --pad-oper --unpad-paren Src/editlib/*.cpp
