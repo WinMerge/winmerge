14d9e32f507a9c55dc6b06e70b45f970839a9ce5
Tim Gerundt <tim@gerundt.de>
2009-10-19 20:28:49 +0000

    Web: Add quick tour

--- Web/docs/index.php
+++ Web/docs/index.php
@@ -3,12 +3,14 @@
 
   $page = new Page;
   $page->setDescription(__('Documentation from WinMerge like manual, release notes, change log and Development Wiki.'));
-  $page->setKeywords(__('WinMerge, documentation, manual, release notes, known issues, change log, Development Wiki'));
+  $page->setKeywords(__('WinMerge, documentation, manual, quick tour, release notes, known issues, change log, Development Wiki'));
   $page->printHead(__('Documentation'), TAB_DOCS);
 
   $page->printHeading(__('Documentation'));
   $page->printLinkedSubHeading(__('Manual'), 'manual/');
   $page->printPara(__('The <a href="%s">manual</a> explains how to use WinMerge, and documents its capabilities and limitations.', 'manual/'));
+  $page->printLinkedSubHeading(__('Quick Tour'), 'tour/');
+  $page->printPara(__('The <a href="%s">quick tour</a> describe the main  features from WinMerge at one page.', 'tour/'));
   $page->printLinkedSubHeading(__('Release Notes'), 'releasenotes.php');
   $page->printPara(__('The <a href="%1$s">release notes</a> are a short summary of important changes, enhancements, bug fixes and <a href="%2$s">known issues</a> in the current WinMerge release.', 'releasenotes.php', 'releasenotes.php#issues'));
   $page->printLinkedSubHeading(__('Change Log'), 'changelog.php');
