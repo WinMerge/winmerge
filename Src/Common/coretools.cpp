b9c2c1f6c3383a406d11d789391898d876cab627
Jochen Tucht <jtuc@users.sourceforge.net>
2010-05-16 13:45:23 +0000

    VC2010 does not accept a dot to serve as a class resolution operator

--- Src/Common/coretools.cpp
+++ Src/Common/coretools.cpp
@@ -270,7 +270,7 @@ void SplitViewName(LPCTSTR s, String * path, String * name, String * ext)
 {
 	String sViewName(s);
 	size_t nOffset = sViewName.find(_T("@@"));
-	if (nOffset != std::string::npos)
+	if (nOffset != String::npos)
 	{
 		sViewName.erase(nOffset);
 		SplitFilename(sViewName.c_str(), path, name, ext);
@@ -765,7 +765,7 @@ void GetDecoratedCmdLine(String sCmdLine, String &sDecoratedCmdLine,
 		sCmdLine.erase(clpos, clpos - sCmdLine.length());
 
 	std::string::size_type pos = sCmdLine.find(_T(" "));
-	if (pos != std::string.npos)
+	if (pos != String::npos)
 	{
 		// First space was before switch, we don't need "s
 		// (executable path didn't contain spaces)
@@ -785,7 +785,7 @@ void GetDecoratedCmdLine(String sCmdLine, String &sDecoratedCmdLine,
 			prevPos = pos;
 			pos = sCmdLine.find(_T(" "), prevPos + 1);
 
-			if (pos != std::string.npos)
+			if (pos != String::npos)
 			{
 				if (sCmdLine[pos + 1] == '/' || sCmdLine[pos + 1] == '-')
 				{
@@ -800,7 +800,7 @@ void GetDecoratedCmdLine(String sCmdLine, String &sDecoratedCmdLine,
 
 		if (addQuote)
 		{
-			if (pos != std::string.npos)
+			if (pos != String::npos)
 			{
 				sExecutable = sCmdLine.substr(0, pos);
 				sDecoratedCmdLine += sExecutable;
