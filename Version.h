a18bb3d018abeb18430db68f25acbb057ba17263
sdottaka <none@none>
2011-02-28 04:30:08 +0900

    Update version numbers

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,13,13,9
-#define PRODUCTVER     2,13,13,9
-#define STRFILEVER     "2.13.13.9+-jp-9"
-#define STRPRODUCTVER  "2.13.13.9+-jp-9"
+#define FILEVER        2,13,13,10
+#define PRODUCTVER     2,13,13,10
+#define STRFILEVER     "2.13.13.10+-jp-10"
+#define STRPRODUCTVER  "2.13.13.10+-jp-10"
