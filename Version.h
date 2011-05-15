51372a702d87df7c39164f2ef94e2346654f8bf1
sdottaka <none@none>
2011-05-16 00:39:46 +0900

    Update revision number

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,13,13,13
-#define PRODUCTVER     2,13,13,13
-#define STRFILEVER     "2.13.13.13+-jp-13"
-#define STRPRODUCTVER  "2.13.13.13+-jp-13"
+#define FILEVER        2,13,20,1
+#define PRODUCTVER     2,13,20,1
+#define STRFILEVER     "2.13.20.1+-jp-1"
+#define STRPRODUCTVER  "2.13.20.1+-jp-1"
