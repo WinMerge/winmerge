0c7d8bdfa7ae4590dd8bdf9c0fc2b9bc30c08b50
sdottaka <none@none>
2013-02-03 23:04:57 -0800

    * Make ShellExtension icon background transparent on Vista or later * Remove WinMergeParent bitmap

--- ShellExtension/Resource.h
+++ ShellExtension/Resource.h
@@ -16,13 +16,12 @@
 #define IDS_RESELECT_FIRST              110
 #define IDB_WINMERGE                    202
 #define IDB_WINMERGEDIR                 203
-#define IDB_WINMERGEPARENT              204
 
 // Next default values for new objects
 //
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        205
+#define _APS_NEXT_RESOURCE_VALUE        204
 #define _APS_NEXT_COMMAND_VALUE         32768
 #define _APS_NEXT_CONTROL_VALUE         201
 #define _APS_NEXT_SYMED_VALUE           111
