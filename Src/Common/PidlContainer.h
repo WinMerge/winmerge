bc980a08f6f67c7bb609cbca9d1908b51e9fbf60
sdottaka <none@none>
2013-04-21 22:32:39 +0900

    Cppcheck: The extra qualification '*::' is unnecessary and is considered an error by many compilers.

--- Src/Common/PidlContainer.h
+++ Src/Common/PidlContainer.h
@@ -64,7 +64,7 @@ public:
 	 * @return Pointer to array of LPCITEMIDLIST items.
 	 *		   May be invalidated by Add()
 	 */
-	LPCITEMIDLIST* CPidlContainer::GetList() const;
+	LPCITEMIDLIST* GetList() const;
 
 	/**
 	 * @brief Returns number of items in container
