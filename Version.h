5301240999d83f0eabf312b22f73c28fd9a17e59
sdottaka <none@none>
2013-05-20 01:36:05 +0900

    Update revisions

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,7
-#define PRODUCTVER     2,14,0,7
-#define STRFILEVER     "2.14.0.7+-jp-7"
-#define STRPRODUCTVER  "2.14.0.7+-jp-7"
+#define FILEVER        2,14,0,8
+#define PRODUCTVER     2,14,0,8
+#define STRFILEVER     "2.14.0.8+-jp-8"
+#define STRPRODUCTVER  "2.14.0.8+-jp-8"
