190cd45e2f87849dbcedd1274a784779dcf0588f
sdottaka <none@none>
2013-05-29 23:17:12 +0900

    BuildBin.cmd: fix for 32bit OS

--- BuildBin.cmd
+++ BuildBin.cmd
@@ -14,12 +14,14 @@ for %%i in (Filters\*.flt Filters\*.tmpl Filters\*.txt) do (
 
 setlocal
 call "%VS100COMNTOOLS%vsvars32.bat"
-MSBuild WinMerge_vc10.sln /t:Rebuild /p:Configuration="Release Unicode" /p:Platform="Win32"
-MSBuild WinMerge_vc10.sln /t:Rebuild /p:Configuration="Release Unicode" /p:Platform="x64"
+MSBuild WinMerge_vc10.sln /t:Rebuild /p:Configuration="Release Unicode" /p:Platform="Win32" || pause
+MSBuild WinMerge_vc10.sln /t:Rebuild /p:Configuration="Release Unicode" /p:Platform="x64" || pause
 endlocal
 
+for %%i in ("%ProgramFiles(x86)%" "%ProgramFiles%") do (
+  if exist "%%~i\Inno Setup 5\iscc.exe" (
+    "%%~i\Inno Setup 5\iscc.exe" "Installer\innosetup\WinMerge.iss" || pause
+    "%%~i\Inno Setup 5\iscc.exe" "Installer\innosetup\WinMergeX64.iss" || pause
+  )
+)
 
-"c:\Program Files (x86)\Inno Setup 5\iscc" "Installer\innosetup\WinMerge.iss"
-"c:\Program Files (x86)\Inno Setup 5\iscc" "Installer\innosetup\WinMergeX64.iss"
-
-pause
