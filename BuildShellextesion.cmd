1a4a591855fd3793abfa5d9e699d09a390c3823e
sdottaka <none@none>
2013-05-29 22:57:23 +0900

    Remove unused files

--- BuildShellextesion.cmd
+++
@@ -1,9 +0,0 @@
-cd /d "%~dp0\ShellExtension"
-
-call "%VS71COMNTOOLS%vsvars32.bat"
-devenv.exe /useenv ShellExtension.sln /build "Release MinDependency"
-devenv.exe /useenv ShellExtension.sln /build "Unicode Release MinDependency"
-
-pause
-
-
--- devenvx64.cmd
+++
@@ -1,6 +0,0 @@
-cd /d "%~dp0"
-
-call "%VS71COMNTOOLS%vsvars32.bat"
-call "C:\Program Files\Microsoft Platform SDK\SetEnv.Cmd" /XP64 /RETAIL
-devenv.exe /useenv
-
