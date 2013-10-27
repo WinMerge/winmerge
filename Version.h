eb1e6e4f391148d2985001400a8e8cc231ab7220
sdottaka <none@none>
2013-10-27 23:12:51 +0900

    Update revisions
    --HG--
    branch : stable

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,18
-#define PRODUCTVER     2,14,0,18
-#define STRFILEVER     "2.14.0.18+-jp-18"
-#define STRPRODUCTVER  "2.14.0.18+-jp-18"
+#define FILEVER        2,14,0,19
+#define PRODUCTVER     2,14,0,19
+#define STRFILEVER     "2.14.0.19+-jp-19"
+#define STRPRODUCTVER  "2.14.0.19+-jp-19"
