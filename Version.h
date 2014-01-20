f4eb476bbfe4834fcd768faafdbd2cbd776555e3
sdottaka <none@none>
2014-01-20 23:25:09 +0900

    Bump revision to 2.14.0+-jp-28
    --HG--
    branch : stable

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,27
-#define PRODUCTVER     2,14,0,27
-#define STRFILEVER     "2.14.0.27+-jp-27"
-#define STRPRODUCTVER  "2.14.0.27+-jp-27"
+#define FILEVER        2,14,0,28
+#define PRODUCTVER     2,14,0,28
+#define STRFILEVER     "2.14.0.28+-jp-28"
+#define STRPRODUCTVER  "2.14.0.28+-jp-28"
