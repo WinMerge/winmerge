b0e6ea8f116ec2fafeb02823648ca641cd675b01
sdottaka <sdottaka@sourceforge.net>
2014-03-17 00:56:13 +0900

    Add "Recent Files Or Folders" menu item to [File] menu
    --HG--
    branch : stable

--- Src/JumpList.h
+++ Src/JumpList.h
@@ -8,11 +8,21 @@
 #define _JUMPLIST_H_
 
 #include "UnicodeString.h"
+#include <vector>
 
 namespace JumpList
 {
+	struct Item
+	{
+		Item(const String& path, const String& params, const String& title, const String& desc): path(path), params(params), title(title), desc(desc) {}
+		String path;
+		String params;
+		String desc;
+		String title;
+	};
 	bool SetCurrentProcessExplicitAppUserModelID(const std::wstring& appid);
 	bool AddToRecentDocs(const String& app_path = _T(""), const String& params = _T(""), const String& title = _T(""), const String& desc = _T(""), int icon_index = 0);
+	std::vector<Item> GetRecentDocs(size_t nMaxItems);
 }
 
 #endif
