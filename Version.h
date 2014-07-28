15d047e2ea24130172c78e9af474d39ca8593154
sdottaka <sdottaka@sourceforge.net>
2014-07-29 01:51:30 +0900

    Bump revision to 2.14.0+-jp-49
    --HG--
    branch : stable

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,48
-#define PRODUCTVER     2,14,0,48
-#define STRFILEVER     "2.14.0.48+-jp-48"
-#define STRPRODUCTVER  "2.14.0.48+-jp-48"
+#define FILEVER        2,14,0,49
+#define PRODUCTVER     2,14,0,49
+#define STRFILEVER     "2.14.0.49+-jp-49"
+#define STRPRODUCTVER  "2.14.0.49+-jp-49"
