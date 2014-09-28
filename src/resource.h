43eed0d7fb447e56858cd38f1fb4db35e8d8cc02
sdottaka <sdottaka@sourceforge.net>
2014-09-28 23:57:59 +0900

    Assign Ctrl+0 key to reset zoom

--- src/resource.h
+++ src/resource.h
@@ -72,7 +72,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
 #define _APS_NEXT_RESOURCE_VALUE        131
-#define _APS_NEXT_COMMAND_VALUE         32845
+#define _APS_NEXT_COMMAND_VALUE         32847
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           111
 #endif
