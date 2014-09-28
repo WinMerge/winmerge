3be6a7dc7221c15b89266f889404b8c1cef8005c
sdottaka <sdottaka@sourceforge.net>
2014-09-29 01:39:57 +0900

    CImgWindow.hpp: - Change background color   - Draw margins for image

--- src/CImgWindow.hpp
+++ src/CImgWindow.hpp
@@ -22,6 +22,7 @@
 
 class CImgWindow
 {
+	enum { MARGIN = 16 };
 public:
 	CImgWindow() :
 		  m_fip(NULL)
@@ -89,12 +90,12 @@ public:
 		RECT rc;
 		GetClientRect(m_hWnd, &rc);
 
-		if (rc.right - rc.left < m_fip->getWidth() * m_zoom)
-			lp.x = static_cast<int>((dx + m_nHScrollPos) / m_zoom);
+		if (rc.right - rc.left < m_fip->getWidth() * m_zoom + MARGIN * 2)
+			lp.x = static_cast<int>((dx + m_nHScrollPos) / m_zoom) - MARGIN;
 		else
 			lp.x = static_cast<int>((dx - (rc.right / 2 - m_fip->getWidth() / 2 * m_zoom)) / m_zoom);
-		if (rc.bottom - rc.top < m_fip->getHeight() * m_zoom)
-			lp.y = static_cast<int>((dy + m_nVScrollPos) / m_zoom);
+		if (rc.bottom - rc.top < m_fip->getHeight() * m_zoom + MARGIN * 2)
+			lp.y = static_cast<int>((dy + m_nVScrollPos) / m_zoom) - MARGIN;
 		else
 			lp.y = static_cast<int>((dy - (rc.bottom / 2 - m_fip->getHeight() / 2 * m_zoom)) / m_zoom);
 		return lp;
@@ -118,23 +119,19 @@ public:
 		RECT rc;
 		GetClientRect(m_hWnd, &rc);
 
-		if (rc.right - rc.left < m_fip->getWidth() * m_zoom)
+		if (rc.right - rc.left < m_fip->getWidth() * m_zoom + MARGIN * 2)
 		{
-			if (x * m_zoom < m_nHScrollPos)
-				m_nHScrollPos = static_cast<int>(x * m_zoom - rc.right / 2);
-			else if (m_nHScrollPos + rc.right < x * m_zoom)
-				m_nHScrollPos = static_cast<int>(x * m_zoom - rc.right / 2);
+			if (x * m_zoom + MARGIN < m_nHScrollPos || m_nHScrollPos + rc.right < x * m_zoom + MARGIN)
+				m_nHScrollPos = static_cast<int>(x * m_zoom + MARGIN - rc.right / 2);
 			if (m_nHScrollPos < 0)
 				m_nHScrollPos = 0;
 			else if (m_nHScrollPos > sih.nMax - static_cast<int>(sih.nPage))
 				m_nHScrollPos = sih.nMax - sih.nPage;
 		}
-		if (rc.bottom - rc.top < m_fip->getHeight() * m_zoom)
+		if (rc.bottom - rc.top < m_fip->getHeight() * m_zoom + MARGIN * 2)
 		{
-			if (y * m_zoom < m_nVScrollPos)
-				m_nVScrollPos = static_cast<int>(y * m_zoom - rc.bottom / 2);
-			else if (m_nVScrollPos + rc.bottom < y * m_zoom)
-				m_nVScrollPos = static_cast<int>(y * m_zoom - rc.bottom / 2);
+			if (y * m_zoom + MARGIN < m_nVScrollPos || m_nVScrollPos + rc.bottom < y * m_zoom + MARGIN)
+				m_nVScrollPos = static_cast<int>(y * m_zoom + MARGIN - rc.bottom / 2);
 			if (m_nVScrollPos < 0)
 				m_nVScrollPos = 0;
 			else if (m_nVScrollPos > siv.nMax - static_cast<int>(siv.nPage))
@@ -192,6 +189,14 @@ public:
 		m_nHScrollPos = static_cast<int>(m_nHScrollPos / oldZoom * m_zoom);
 		if (m_fip)
 		{
+			RECT rc;
+			GetClientRect(m_hWnd, &rc);
+			unsigned width  = static_cast<unsigned>(m_fip->getWidth()  * m_zoom) + MARGIN * 2; 
+			unsigned height = static_cast<unsigned>(m_fip->getHeight() * m_zoom) + MARGIN * 2; 
+			if (m_nHScrollPos > width  - rc.right)
+				m_nHScrollPos = width  - rc.right;
+			if (m_nVScrollPos > height - rc.bottom)
+				m_nVScrollPos = height - rc.bottom;
 			CalcScrollBarRange();
 			InvalidateRect(m_hWnd, NULL, TRUE);
 		}
@@ -213,15 +218,18 @@ private:
 	ATOM MyRegisterClass(HINSTANCE hInstance)
 	{
 		WNDCLASSEXW wcex = {0};
-		wcex.cbSize         = sizeof(WNDCLASSEX); 
-		wcex.style			= CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;
-		wcex.lpfnWndProc	= (WNDPROC)WndProc;
-		wcex.cbClsExtra		= 0;
-		wcex.cbWndExtra		= 0;
-		wcex.hInstance		= hInstance;
-		wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
-		wcex.hbrBackground	= (HBRUSH)(COLOR_WINDOW+1);
-		wcex.lpszClassName	= L"WinImgWindowClass";
+		if (!GetClassInfoEx(hInstance, L"WinImgWindowClass", &wcex))
+		{
+			wcex.cbSize         = sizeof(WNDCLASSEX); 
+			wcex.style			= CS_DBLCLKS | CS_HREDRAW | CS_VREDRAW;
+			wcex.lpfnWndProc	= (WNDPROC)WndProc;
+			wcex.cbClsExtra		= 0;
+			wcex.cbWndExtra		= 0;
+			wcex.hInstance		= hInstance;
+			wcex.hCursor		= LoadCursor(NULL, IDC_ARROW);
+			wcex.hbrBackground	= CreateSolidBrush(RGB(206, 215, 230));
+			wcex.lpszClassName	= L"WinImgWindowClass";
+		}
 		return RegisterClassExW(&wcex);
 	}
 
@@ -233,30 +241,29 @@ private:
 		{
 			RECT rcImg, rc;
 			GetClientRect(m_hWnd, &rc);
-			if (rc.right - rc.left > m_fip->getWidth() * m_zoom)
+			if (rc.right - rc.left > m_fip->getWidth() * m_zoom + MARGIN * 2)
 			{
 				rcImg.left  = static_cast<int>(((rc.right - rc.left) - m_fip->getWidth() * m_zoom) / 2); 
 				rcImg.right = static_cast<int>(rcImg.left + m_fip->getWidth() * m_zoom); 
 			}
 			else
 			{
-				rcImg.left = -m_nHScrollPos;
-				rcImg.right = static_cast<int>(m_fip->getWidth() * m_zoom - m_nHScrollPos);
+				rcImg.left = -m_nHScrollPos + MARGIN;
+				rcImg.right = static_cast<int>(m_fip->getWidth() * m_zoom + MARGIN - m_nHScrollPos);
 			}
-			if (rc.bottom - rc.top > m_fip->getHeight() * m_zoom)
+			if (rc.bottom - rc.top > m_fip->getHeight() * m_zoom + MARGIN * 2)
 			{
 				rcImg.top    = static_cast<int>(((rc.bottom - rc.top) - m_fip->getHeight() * m_zoom) / 2); 
 				rcImg.bottom = static_cast<int>(rcImg.top + m_fip->getHeight() * m_zoom); 
 			}
 			else
 			{
-				rcImg.top    = -m_nVScrollPos;
-				rcImg.bottom = static_cast<int>(m_fip->getHeight() * m_zoom - m_nVScrollPos);
+				rcImg.top    = -m_nVScrollPos + MARGIN;
+				rcImg.bottom = static_cast<int>(m_fip->getHeight() * m_zoom + MARGIN - m_nVScrollPos);
 			}
-			m_fip->drawEx(hdc, rcImg, false, m_useBackColor ? &m_backColor : NULL);
-			
-			RECT rcBorder = {rcImg.left - 2, rcImg.top - 2, rcImg.right + 2, rcImg.bottom + 2};
-			::DrawEdge(hdc, &rcBorder, BDR_RAISEDOUTER | BDR_SUNKENINNER, BF_RECT);
+
+			if (m_fip->isValid())
+				m_fip->drawEx(hdc, rcImg, false, m_useBackColor ? &m_backColor : NULL);
 			
 			if (GetFocus() == m_hWnd)
 			{
@@ -369,7 +376,7 @@ private:
 			GetClientRect(m_hWnd, &rc);
 			if (!(nFlags & MK_SHIFT))
 			{
-				if (rc.bottom - rc.top < m_fip->getHeight() * m_zoom)
+				if (rc.bottom - rc.top < m_fip->getHeight() * m_zoom + MARGIN * 2)
 				{
 					SCROLLINFO si = {0};
 					si.cbSize = sizeof SCROLLINFO;
@@ -387,7 +394,7 @@ private:
 			}
 			else
 			{
-				if (rc.right - rc.left < m_fip->getWidth() * m_zoom)
+				if (rc.right - rc.left < m_fip->getWidth() * m_zoom + MARGIN * 2)
 				{
 					SCROLLINFO si = {0};
 					si.cbSize = sizeof SCROLLINFO;
@@ -471,8 +478,8 @@ private:
 		si.cbSize = sizeof(SCROLLINFO);
 		if (m_fip)
 		{
-			unsigned width  = static_cast<unsigned>(m_fip->getWidth()  * m_zoom);
-			unsigned height = static_cast<unsigned>(m_fip->getHeight() * m_zoom); 
+			unsigned width  = static_cast<unsigned>(m_fip->getWidth()  * m_zoom) + MARGIN * 2;
+			unsigned height = static_cast<unsigned>(m_fip->getHeight() * m_zoom) + MARGIN * 2; 
 
 			si.fMask = SIF_POS | SIF_RANGE | SIF_PAGE | SIF_DISABLENOSCROLL;
 			si.nMin = 0;
