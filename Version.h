6d0ab189779b5cf2cb3e2838e64a2f191ebde70b
sdottaka <sdottaka@users.sourceforge.net>
2014-11-12 01:27:28 +0900

    Bump revision to 2.14.0+-jp-61
    --HG--
    branch : stable

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,60
-#define PRODUCTVER     2,14,0,60
-#define STRFILEVER     "2.14.0.60+-jp-60"
-#define STRPRODUCTVER  "2.14.0.60+-jp-60"
+#define FILEVER        2,14,0,61
+#define PRODUCTVER     2,14,0,61
+#define STRFILEVER     "2.14.0.61+-jp-61"
+#define STRPRODUCTVER  "2.14.0.61+-jp-61"
