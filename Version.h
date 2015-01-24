cac4445d0435e03511d802e962cd631af271625f
sdottaka <sdottaka@users.sourceforge.net>
2015-01-24 17:10:16 +0900

    Update revision to 2.14.0+-jp-68
    --HG--
    branch : stable

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,67
-#define PRODUCTVER     2,14,0,67
-#define STRFILEVER     "2.14.0.67+-jp-67"
-#define STRPRODUCTVER  "2.14.0.67+-jp-67"
+#define FILEVER        2,14,0,68
+#define PRODUCTVER     2,14,0,68
+#define STRFILEVER     "2.14.0.68+-jp-68"
+#define STRPRODUCTVER  "2.14.0.68+-jp-68"
