d561621928b35be56a7bc46d35d2e26238053d1f
sdottaka <sdottaka@users.sourceforge.net>
2015-05-16 20:18:06 +0900

    crystaledit: reduce compile warnings
    --HG--
    branch : stable

--- Externals/crystaledit/editlib/po.cpp
+++ Externals/crystaledit/editlib/po.cpp
@@ -95,7 +95,7 @@ ParseLinePo (DWORD dwCookie, int nLineIndex, TEXTBLOCK * pBuf, int &nActualItems
   int nIdentBegin = -1;
   int nPrevI = -1;
   int I=0;
-  for (I = 0;; nPrevI = I, I = CharNext(pszChars+I) - pszChars)
+  for (I = 0;; nPrevI = I, I = static_cast<int>(::CharNext(pszChars+I) - pszChars))
     {
       if (I == nPrevI)
         {
