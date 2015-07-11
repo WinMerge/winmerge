4851362aef654bb54e99ecbd26d86b9283afde63
sdottaka <sdottaka@users.sourceforge.net>
2015-07-11 13:19:42 +0900

    Use Mecurial EOL Extension
    --HG--
    branch : stable

--- BuildManual.cmd
+++ BuildManual.cmd
@@ -1,11 +1,4 @@
 mkdir Build\Docs 2> NUL
-for %%i in (Docs\Users\*.txt Src\COPYING) do (
-  cscript convertlf2crlf.vbs "%%i" Build\Docs\%%~nxi"
-)
-
-for %%i in (Translations\Docs\Readme\Readme-Japanese.txt) do (
-  cscript convertlf2crlf.vbs "%%i" Build\Docs\%%~nxi"
-)
 
 cd Docs\Users\Manual\build
 call build_htmlhelp.bat
