45059fced2f61b8f82e5a9c75a3ceff7aa985122
sdottaka <sdottaka@users.sourceforge.net>
2015-09-13 19:40:13 +0900

    Bump revision to 2.14.0+-jp-85

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,84
-#define PRODUCTVER     2,14,0,84
-#define STRFILEVER     "2.14.0.84+-jp-84"
-#define STRPRODUCTVER  "2.14.0.84+-jp-84"
+#define FILEVER        2,14,0,85
+#define PRODUCTVER     2,14,0,85
+#define STRFILEVER     "2.14.0.85+-jp-85"
+#define STRPRODUCTVER  "2.14.0.85+-jp-85"
