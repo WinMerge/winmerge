20172500923e71ba6261d6d72a734222f8ea0ba1
sdottaka <sdottaka@users.sourceforge.net>
2015-09-20 12:49:38 +0900

    LanguageSelect.h: Remove unused #include

--- Src/Common/LanguageSelect.h
+++ Src/Common/LanguageSelect.h
@@ -8,7 +8,6 @@
 #include <vector>
 #include <string>
 #include <map>
-#include "CMoveConstraint.h"
 
 /////////////////////////////////////////////////////////////////////////////
 // CLanguageSelect class
