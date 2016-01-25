bf06ea942a3fc516916f9fbda4be9fcf66efd1f1
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2016-01-25 23:53:34 +0900

    Translations: Use unique ID instead of line number to identify resource strings.               This makes commit size small when modified the Merge.rc file.

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,97
-#define PRODUCTVER     2,14,0,97
-#define STRFILEVER     "2.14.0.97+-jp-97"
-#define STRPRODUCTVER  "2.14.0.97+-jp-97"
+#define FILEVER        2,14,0,98
+#define PRODUCTVER     2,14,0,98
+#define STRFILEVER     "2.14.0.98+-jp-98"
+#define STRPRODUCTVER  "2.14.0.98+-jp-98"
