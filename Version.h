d4c9b93f1374cfc7f1fa950238af4dc7dc923a17
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2016-06-12 21:23:34 +0900

    Bump revision to 2.14.0+-jp-108

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,107
-#define PRODUCTVER     2,14,0,107
-#define STRFILEVER     "2.14.0.107+-jp-107"
-#define STRPRODUCTVER  "2.14.0.107+-jp-107"
+#define FILEVER        2,14,0,108
+#define PRODUCTVER     2,14,0,108
+#define STRFILEVER     "2.14.0.108+-jp-108"
+#define STRPRODUCTVER  "2.14.0.108+-jp-108"
