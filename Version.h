51be08788816c688f83636c9ce215cc6911decab
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2016-08-28 19:47:34 +0900

    Bump revision to 2.14.0+-jp-116

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,115
-#define PRODUCTVER     2,14,0,115
-#define STRFILEVER     "2.14.0.115+-jp-115"
-#define STRPRODUCTVER  "2.14.0.115+-jp-115"
+#define FILEVER        2,14,0,116
+#define PRODUCTVER     2,14,0,116
+#define STRFILEVER     "2.14.0.116+-jp-116"
+#define STRPRODUCTVER  "2.14.0.116+-jp-116"
