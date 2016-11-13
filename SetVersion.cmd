6ef54f8855c0ce06d8d3348e9045e14933d9e833
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2016-11-13 18:18:56 +0900

    Bump revision to 1.0.4.0

--- SetVersion.cmd
+++ SetVersion.cmd
@@ -1,4 +1,4 @@
 set MAJOR=1
 set MINOR=0
-set REVISION=3
+set REVISION=4
 set PATCHLEVEL=0
--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        1,0,3,0
-#define PRODUCTVER     1,0,3,0
-#define STRFILEVER     "1.0.3.0"
-#define STRPRODUCTVER  "1.0.3.0"
+#define FILEVER        1,0,4,0
+#define PRODUCTVER     1,0,4,0
+#define STRFILEVER     "1.0.4.0"
+#define STRPRODUCTVER  "1.0.4.0"
