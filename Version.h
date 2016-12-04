64cbe29bab61cfed4bca3828f278b7aa51cbc620
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2016-12-04 22:38:10 +0900

    Bump revision to 2.14.0+-jp-124

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,123
-#define PRODUCTVER     2,14,0,123
-#define STRFILEVER     "2.14.0.123+-jp-123"
-#define STRPRODUCTVER  "2.14.0.123+-jp-123"
+#define FILEVER        2,14,0,124
+#define PRODUCTVER     2,14,0,124
+#define STRFILEVER     "2.14.0.124+-jp-124"
+#define STRPRODUCTVER  "2.14.0.124+-jp-124"
