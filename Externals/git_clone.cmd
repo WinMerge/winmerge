b042c49c0c78e13f4a06d70cf1a766036a86b3dc
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2016-12-30 00:07:39 +0900

    Add git_clone.cmd and hg_clone.cmd

---
+++ Externals/git_clone.cmd
@@ -0,0 +1,3 @@
+for %%i in (freeimage frhed patch sevenzip winimerge) do (
+  git clone https://github.com/sdottaka/%%i.git
+)
---
+++ Externals/hg_clone.cmd
@@ -0,0 +1,3 @@
+for %%i in (freeimage frhed patch sevenzip winimerge) do (
+  hg clone https://bitbucket.org/sdottaka/%%i
+)
