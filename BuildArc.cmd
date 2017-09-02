8750e50255dc06e95b50f58fd4d9edceaf1b8037
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2017-09-02 15:37:18 +0900

    UploadToGithub.cmd: Add virualtotal.com's scan result URL to release description

--- BuildArc.cmd
+++ BuildArc.cmd
@@ -156,4 +156,10 @@ for /F "delims=" %%f in ('type "%DISTDIR%\jp_files.txt"') do (
   "%ProgramFiles(x86)%\VirusTotalUploader2\VirusTotalUploader2.2.exe" %%f
 )
 
+@echo off
+echo.
+for /F "delims=" %%f in ('type "%DISTDIR%\jp_files.txt"') do (
+  for /F %%h in ('certutil -hashfile %%f SHA256 ^| findstr -v hash') do echo %%~nxf: https://www.virustotal.com/en/file/%%h/analysis/
+)
+
 pause
