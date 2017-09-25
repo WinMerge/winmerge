6cf262c50ffecd85aac6a57956e7bb3975c501ae
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2017-09-26 01:08:58 +0900

    Bump revision to 2.14.0+-jp-141

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,140
-#define PRODUCTVER     2,14,0,140
-#define STRFILEVER     "2.14.0.140+-jp-140"
-#define STRPRODUCTVER  "2.14.0.140+-jp-140"
+#define FILEVER        2,14,0,141
+#define PRODUCTVER     2,14,0,141
+#define STRFILEVER     "2.14.0.141+-jp-141"
+#define STRPRODUCTVER  "2.14.0.141+-jp-141"
