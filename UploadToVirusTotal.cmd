553dbb68f1318aa965b50d8bec248ff0d7b9b789
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2017-09-29 23:41:57 +0900

    Restore URL

--- UploadToVirusTotal.cmd
+++ UploadToVirusTotal.cmd
@@ -1,8 +1,8 @@
 cd /d "%~dp0"
 
-set DISTDIR=c:\web page\geocities
+set DISTDIR=.\Build\Releases
 
-for /F "delims=" %%f in ('type "%DISTDIR%\jp_files.txt"') do (
+for /F "delims=" %%f in ('type "%DISTDIR%\files.txt"') do (
   "%ProgramFiles(x86)%\VirusTotalUploader2\VirusTotalUploader2.2.exe" %%f
 )
 
