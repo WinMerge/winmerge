4b1811cb762c1c597895b0c3937b28a8d72302c2
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2017-09-30 05:14:02 +0900

    temporarily set revision to 2.15.2.0

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,141
-#define PRODUCTVER     2,14,0,141
-#define STRFILEVER     "2.14.0.141+-jp-141"
-#define STRPRODUCTVER  "2.14.0.141+-jp-141"
+#define FILEVER        2,15,2,0
+#define PRODUCTVER     2,15,2,0
+#define STRFILEVER     "2.15.2.0"
+#define STRPRODUCTVER  "2.15.2.0"
