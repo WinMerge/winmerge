176064b77981795415e183ec95d63e0da7378189
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2017-10-01 22:21:32 +0900

    Fix the revisions of external sources

--- Externals/hg_clone.cmd
+++ Externals/hg_clone.cmd
@@ -1,3 +1,5 @@
-for %%i in (freeimage frhed patch sevenzip winimerge) do (
-  hg clone https://bitbucket.org/sdottaka/%%i
-)
+hg clone https://bitbucket.org/sdottaka/freeimage -u 05e4e65
+hg clone https://bitbucket.org/sdottaka/winimerge -u 1.0.7.0
+hg clone https://bitbucket.org/sdottaka/sevenzip -u 1510f74
+hg clone https://bitbucket.org/sdottaka/frhed -u 43fbdca
+hg clone https://bitbucket.org/sdottaka/patch -u b0a7238
