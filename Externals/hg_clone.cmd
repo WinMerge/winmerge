c52852dc14b2c8275d85070463241a317eefb646
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2017-10-14 19:36:47 +0900

    hg_clone.cmd: pull and update if already cloned

--- Externals/hg_clone.cmd
+++ Externals/hg_clone.cmd
@@ -1,5 +1,17 @@
-hg clone https://bitbucket.org/sdottaka/freeimage -u 05e4e65
-hg clone https://bitbucket.org/sdottaka/winimerge -u 1.0.7.0
-hg clone https://bitbucket.org/sdottaka/sevenzip -u 1510f74
-hg clone https://bitbucket.org/sdottaka/frhed -u d00b64b
-hg clone https://bitbucket.org/sdottaka/patch -u b0a7238
+set urls_revs=^
+  https://bitbucket.org/sdottaka/freeimage!05e4e65 ^
+  https://bitbucket.org/sdottaka/winimerge!1.0.7.0 ^
+  https://bitbucket.org/sdottaka/sevenzip!1510f74 ^
+  https://bitbucket.org/sdottaka/frhed!d00b64b ^
+  https://bitbucket.org/sdottaka/patch!b0a7238
+
+pushd "%~dp0"
+for %%i in (%urls_revs%) do (
+  for /F "tokens=1,2 delims=!" %%j in ("%%i") do (
+    if not exist %%~nj\.hg hg clone %%j
+    pushd %%~nj
+    hg pull -u -r %%k
+    popd
+  )
+)
+popd
