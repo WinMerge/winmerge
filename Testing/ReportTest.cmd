2038d2765acfcb16b1878e9e9c5fb8030b926268
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2017-11-08 21:15:28 +0900

    DirCmpReportDlg.cpp: Use OptionsMgr

--- Testing/ReportTest.cmd
+++ Testing/ReportTest.cmd
@@ -6,9 +6,9 @@ set ctime=%ctime: =%
 set outputdir=%~dp0\..\BuildTmp\ReportTest\%cdate%-%ctime%
 echo %outputdir%
 mkdir %outputdir%
-for /L %%i in (1,1,10) do ( 
+for /L %%i in (989,1,999) do ( 
   set cnt=%%i
-  set /a lrev=999-!cnt!
-  set /a rrev=1000-!cnt!
-  hg --config extensions.extdiff= --config extdiff.cmd.winmerge=%~dp0\..\Build\x64\MergeUnicodeDebug\WinMergeU.exe --config extdiff.opts.winmerge="-minimize -noninteractive -noprefs -cfg Settings/DirViewExpandSubdirs=1 -r -u -or %outputdir%\%%i.html" winmerge  -r !lrev! -r !rrev!
+  set /a lrev=cnt
+  set /a rrev=cnt+1
+  hg --config extensions.extdiff= --config extdiff.cmd.winmerge=%~dp0\..\Build\x64\MergeUnicodeDebug\WinMergeU.exe --config extdiff.opts.winmerge="-minimize -noninteractive -noprefs -cfg Settings/DirViewExpandSubdirs=1 -cfg ReportFiles/ReportType=2 -cfg ReportFiles/IncludeFileCmpReport=1 -r -u -or %outputdir%\%%i.html" winmerge  -r !lrev! -r !rrev!
 )
