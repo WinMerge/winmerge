9abd9e4a30fe2d7d052922eea931e39445a6638c
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2017-12-13 22:29:14 +0900

    Bump revision to 2.14.0+-jp-143
    --HG--
    branch : jp

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,14,0,142
-#define PRODUCTVER     2,14,0,142
-#define STRFILEVER     "2.14.0.142+-jp-142"
-#define STRPRODUCTVER  "2.14.0.142+-jp-142"
+#define FILEVER        2,14,0,143
+#define PRODUCTVER     2,14,0,143
+#define STRFILEVER     "2.14.0.143+-jp-143"
+#define STRPRODUCTVER  "2.14.0.143+-jp-143"
