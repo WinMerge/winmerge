70b2aa934d3140826b08bad4ab77381ea16f0f0a
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2018-01-08 22:03:14 +0900

    Remove unused declarations
    --HG--
    branch : jp

--- Src/LocationView.h
+++ Src/LocationView.h
@@ -123,7 +123,6 @@ protected:
 	afx_msg void OnVScroll (UINT nSBCode, UINT nPos, CScrollBar * pScrollBar);
 	afx_msg void OnSize(UINT nType, int cx, int cy);
 	afx_msg BOOL OnEraseBkgnd(CDC* pDC);
-	afx_msg void OnPaint();
 	afx_msg void OnSetFocus(CWnd* pOldWnd);
 	afx_msg void OnUpdateFileSave(CCmdUI* pCmdUI);
 	afx_msg void OnUpdateFileSaveLeft(CCmdUI* pCmdUI);
