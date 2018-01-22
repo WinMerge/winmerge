7bbbc1e7f3863468ee78f08715938e175ea3d9b8
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2018-01-23 00:44:12 +0900

    Move repositories to https://bitbucket.org/winmerge/

--- Externals/hg_clone.cmd
+++ Externals/hg_clone.cmd
@@ -1,9 +1,9 @@
 set urls_revs=^
-  https://bitbucket.org/sdottaka/freeimage!05e4e65 ^
-  https://bitbucket.org/sdottaka/winimerge!1.0.7.0 ^
-  https://bitbucket.org/sdottaka/sevenzip!1510f74 ^
-  https://bitbucket.org/sdottaka/frhed!d00b64b ^
-  https://bitbucket.org/sdottaka/patch!b0a7238
+  https://bitbucket.org/winmerge/freeimage!05e4e65 ^
+  https://bitbucket.org/winmerge/winimerge!1.0.7.0 ^
+  https://bitbucket.org/winmerge/sevenzip!1510f74 ^
+  https://bitbucket.org/winmerge/frhed!d00b64b ^
+  https://bitbucket.org/winmerge/patch!b0a7238
 
 pushd "%~dp0"
 for %%i in (%urls_revs%) do (
