8cb31099b0fed3c8ccce234640d4397d9003bea1
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2018-04-27 23:06:13 +0900

    Bump revision to 2.15.4

--- Version.h
+++ Version.h
@@ -1,4 +1,4 @@
-#define FILEVER        2,15,3,0
-#define PRODUCTVER     2,15,3,0
-#define STRFILEVER     "2.15.3.0"
-#define STRPRODUCTVER  "2.15.3.0"
+#define FILEVER        2,15,4,0
+#define PRODUCTVER     2,15,4,0
+#define STRFILEVER     "2.15.4.0"
+#define STRPRODUCTVER  "2.15.4.0"
