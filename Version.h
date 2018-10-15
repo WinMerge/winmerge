1839c6a249f2f3c254efa8ef262a2b9455d03a2a
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2018-10-16 01:00:34 +0900

    Fix release build error (2)

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,15,4,0
-#define PRODUCTVER     2,15,4,0
-#define STRFILEVER     "2.15.4.0"
-#define STRPRODUCTVER  "2.15.4.0"
-#define STRPRIVATEBUILD "-"
+#define FILEVER        2,15,5,0
+#define PRODUCTVER     2,15,5,0
+#define STRFILEVER     "2.15.5.0"
+#define STRPRODUCTVER  "2.15.5.0"
+#define STRPRIVATEBUILD ""
