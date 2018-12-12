b8c41c0cd074f5808aba923cd2adf662c98a91ee
GreyMerlin <greymerlin7@gmail.com>
2018-12-12 12:03:17 -0800

    In Preparation for VS2019 (now in Preview) (2)
    * Copy the various *.vs2017.* solution and project files as new *.vs2019.* files

---
+++ BuildBin.vs2019.cmd
@@ -0,0 +1,27 @@
+cd /d "%~dp0"
+
+call SetVersion.cmd
+cscript /nologo ExpandEnvironmenStrings.vbs Version.in > Version.h
+
+setlocal
+for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\microsoft visual studio\installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
+  call "%InstallDir%\Common7\Tools\vsdevcmd.bat %*
+)
+MSBuild WinMerge.vs2019.sln /t:Rebuild /p:Configuration="Release Unicode" /p:Platform="Win32" || pause
+MSBuild WinMerge.vs2019.sln /t:Rebuild /p:Configuration="Release Unicode" /p:Platform="x64" || pause
+endlocal
+
+if exist "%SIGNBAT_PATH%" (
+  call "%SIGNBAT_PATH%" Build\MergeUnicodeRelease\WinMergeU.exe
+  call "%SIGNBAT_PATH%" Build\MergeUnicodeRelease\MergeLang.dll
+  call "%SIGNBAT_PATH%" Build\x64\MergeUnicodeRelease\WinMergeU.exe
+  call "%SIGNBAT_PATH%" Build\x64\MergeUnicodeRelease\MergeLang.dll
+)
+
+mkdir Build\MergeUnicodeRelease\%APPVER% 2> NUL
+mkdir Build\x64\MergeUnicodeRelease\%APPVER% 2> NUL
+copy Build\MergeUnicodeRelease\*.pdb "Build\MergeUnicodeRelease\%APPVER%\"
+copy Build\x64\MergeUnicodeRelease\*.pdb "Build\x64\MergeUnicodeRelease\%APPVER%\"
