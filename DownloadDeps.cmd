2bdeec871f802c4282fe9504e9253ab22d10f97b
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2018-12-19 07:57:53 +0900

    DownloadDeps.cmd: Change download directory to build/WinMergeDownloadDeps

--- DownloadDeps.cmd
+++ DownloadDeps.cmd
@@ -1,9 +1,9 @@
 setlocal
 set path="%ProgramFiles%\7-zip";"%ProgramFiles(x86)%\7-zip";%path%
-set downloadsdir=%temp%\WinMergeDownloadDeps
+set downloadsdir=%~dp0\WinMergeDownloadDeps
 set urls_destdirs=^
 https://bitbucket.org/winmerge/winmerge/downloads/winmerge_manual_build_tools_v1.zip!Docs\Users\Manual\build ^
-https://bitbucket.org/winmerge/winmerge/downloads/ShellExtension-1.17.3.0.zip!Build ^
+https://bitbucket.org/winmerge/winmerge/downloads/ShellExtension-1.17.2.0.zip!Build ^
 https://bitbucket.org/winmerge/winmerge/downloads/Merge7z1805.1-win32.zip!Build ^
 https://bitbucket.org/winmerge/winmerge/downloads/Merge7z1805.1-x64.zip!Build\X64 ^
 https://bitbucket.org/winmerge/frhed/downloads/frhed-0.10904.2017.1-win32.zip!Build ^
@@ -17,7 +17,7 @@ mkdir %downloadsdir% 2> NUL
 for %%p in (%urls_destdirs%) do (
   for /F "tokens=1,2 delims=!" %%u in ("%%p") do (
     if not exist %downloadsdir%\%%~nxu (
-      powershell -command "Invoke-WebRequest %%u -Outfile %downloadsdir%\%%~nxu"
+      powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest %%u -Outfile %downloadsdir%\%%~nxu"
     )
     7z x %downloadsdir%\%%~nxu -aoa -o%%v
   )
