60bf242a89b5dd5bf99c3e5790f15e26174ac7fe
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2018-12-21 08:07:53 +0900

    Build*.cmd: Allow specifying platform in command line parameter
    --HG--
    branch : stable

--- BuildInstaller.cmd
+++ BuildInstaller.cmd
@@ -1,7 +1,17 @@
+if "%1" == "" (
+  call :BuildInstaller
+  call :BuildInstaller x64
+) else (
+  call :BuildInstaller %1 
+)
+goto :eof
+
+:BuildInstaller
+set PLATFORM=%1
 
 for %%i in ("%ProgramFiles(x86)%" "%ProgramFiles%") do (
   if exist "%%~i\Inno Setup 5\iscc.exe" (
-    "%%~i\Inno Setup 5\iscc.exe" "Installer\innosetup\WinMerge.iss" || pause
-    "%%~i\Inno Setup 5\iscc.exe" "Installer\innosetup\WinMergeX64.iss" || pause
+    "%%~i\Inno Setup 5\iscc.exe" "Installer\innosetup\WinMerge%PLATFORM%.iss" || pause
   )
 )
+goto :eof
