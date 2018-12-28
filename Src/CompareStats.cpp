7a0db6dfd6bb46c3e15105abf91235ba4b2097cb
GreyMerlin <greymerlin7@gmail.com>
2018-12-27 17:22:06 -0800

    Repair Compare Statistics dialogs
    * Add proper icon for identical folders
    * Account for identical folders in the Folder column
    * Properly calculate number of Binary Identical and Binary Different items

--- Src/CompareStats.cpp
+++ Src/CompareStats.cpp
@@ -184,19 +184,19 @@ CompareStats::RESULT CompareStats::GetResultFromCode(unsigned diffcode) const
 	else if (di.isResultSame())
 	{
 		// same
-		return di.isBin() ? RESULT_BINSAME : RESULT_SAME;
-	}
-	else
-	{
-		// presumably it is diff
 		if (di.isDirectory())
 		{
-			return RESULT_DIR;
+			return RESULT_DIRSAME;
 		}
 		else
 		{
-			return di.isBin() ? RESULT_BINDIFF : RESULT_DIFF;
+			return di.isBin() ? RESULT_BINSAME : RESULT_SAME;
 		}
+	}
+	else
+	{
+		// presumably it is different
+		return di.isBin() ? RESULT_BINDIFF : RESULT_DIFF;
 	}
 }
 
