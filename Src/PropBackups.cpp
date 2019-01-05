8ff2b4653d8d75e4693a6db556beb94391af71be
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-01-05 22:46:00 +0900

    Fix Warning C26444 Avoid unnamed objects with custom construction and destruction (es.84).

--- Src/PropBackups.cpp
+++ Src/PropBackups.cpp
@@ -65,7 +65,7 @@ void PropBackups::ReadOptions()
  */
 void PropBackups::WriteOptions()
 {
-	strutils::trim_ws(m_sGlobalFolder);
+	m_sGlobalFolder = strutils::trim_ws(m_sGlobalFolder);
 	if (m_sGlobalFolder.length() > 3)
 		m_sGlobalFolder = paths::AddTrailingSlash(m_sGlobalFolder);
 
