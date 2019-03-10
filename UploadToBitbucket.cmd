9139e66b3588ed4bbe2ef89be728fb6861d5c9fb
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-03-10 22:11:27 +0900

    UploadToBitbucket.cmd
    --HG--
    branch : jp

--- UploadToBitbucket.cmd
+++ UploadToBitbucket.cmd
@@ -2,7 +2,7 @@ cd /d "%~dp0"
 
 call SetVersion.cmd
 set DISTDIR=.\build\Releases
-
+set /P BITBUCKET_PASSWORD=Bitbucket password: 
 for /F "delims=" %%f in ('type "%DISTDIR%\files.txt"') do (
   curl -u sdottaka:%BITBUCKET_PASSWORD% -X POST "https://api.bitbucket.org/2.0/repositories/sdottaka/winmerge-v2/downloads" --form files=@"%%f"
 )
