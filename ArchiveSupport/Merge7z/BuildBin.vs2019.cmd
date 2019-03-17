7c2027a97b4a80e9251f11551b30e25e0858d0d5
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-03-18 08:16:44 +0900

    Merge7z: Add BuidBin.*.cmd

---
+++ ArchiveSupport/Merge7z/BuildBin.vs2019.cmd
@@ -0,0 +1,36 @@
+cd /d "%~dp0"
+
+setlocal
+for /f "usebackq tokens=*" %%i in (`"%programfiles(x86)%\microsoft visual studio\installer\vswhere.exe" -latest -products * -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 -property installationPath`) do (
+  set InstallDir=%%i
+)
+if exist "%InstallDir%\Common7\Tools\vsdevcmd.bat" (
+  call "%InstallDir%\Common7\Tools\vsdevcmd.bat
+)
+
+if "%1" == "" (
+  call :BuildBin
+  call :BuildBin x64
+) else (
+  call :BuildBin %1 
+)
+
+goto :eof
+
+:BuildBin
+set PLATFORM=%1
+if "%1" == "" (
+  set PLATFORM_VS=Win32
+) else (
+  set PLATFORM_VS=%1
+)
+MSBuild Merge7z.vs2019.sln /t:Rebuild /p:Configuration="Release" /p:Platform="%PLATFORM_VS%" || pause
+endlocal
+
+if exist "%SIGNBAT_PATH%" (
+  call "%SIGNBAT_PATH%" Build\%PLATFORM%\MergeUnicodeRelease\Merge7z\Merge7z.dll
+)
+
+mkdir Build\%PLATFORM%\MergeUnicodeRelease\%APPVER% 2> NUL
+copy Build\%PlATFORM%\MergeUnicodeRelease\Merge7z\*.pdb "Build\%PLATFORM%\MergeUnicodeRelease\%APPVER%\"
+goto :eof
