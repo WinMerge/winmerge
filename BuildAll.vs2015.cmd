d662ac3e05edfbc60847558b227a4334a6d2773e
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-04-06 19:28:39 +0900

    Remove *.vs2015.* files

--- BuildAll.vs2015.cmd
+++
@@ -1,9 +0,0 @@
-pushd "%~dp0"
-call BuildManual.cmd
-call BuildBin.vs2017.cmd %1
-pushd Testing\GoogleTest\UnitTests
-UnitTests.exe || exit
-popd
-call BuildInstaller.cmd %1
-call BuildArc.cmd %1
-popd
