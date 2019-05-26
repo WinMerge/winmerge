5adee0aca5bdabdc68ca152fa1db7e4b9857b265
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-05-27 08:14:16 +0900

    Bump revision ot 2.16.2+-jp-7
    --HG--
    branch : jp

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,16,2,6
-#define PRODUCTVER     2,16,2,6
-#define STRFILEVER     "2.16.2.6"
-#define STRPRODUCTVER  "2.16.2.6"
-#define STRPRIVATEBUILD "jp-6"
+#define FILEVER        2,16,2,7
+#define PRODUCTVER     2,16,2,7
+#define STRFILEVER     "2.16.2.7"
+#define STRPRODUCTVER  "2.16.2.7"
+#define STRPRIVATEBUILD "jp-7"
