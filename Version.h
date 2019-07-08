3b57d92cd5f0c5834aef5c180da6e7d811f9ca62
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-07-09 08:11:34 +0900

    Bump revision to 2.16.2+-jp-14
    --HG--
    branch : jp

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,16,2,13
-#define PRODUCTVER     2,16,2,13
-#define STRFILEVER     "2.16.2.13"
-#define STRPRODUCTVER  "2.16.2.13"
-#define STRPRIVATEBUILD "jp-13"
+#define FILEVER        2,16,2,14
+#define PRODUCTVER     2,16,2,14
+#define STRFILEVER     "2.16.2.14"
+#define STRPRODUCTVER  "2.16.2.14"
+#define STRPRIVATEBUILD "jp-14"
