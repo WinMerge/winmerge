1b8fe977beba53d0f6150ec25ebff90588e2e98b
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2019-09-01 12:24:47 +0900

    Bump revision to 2.16.4+-jp-2

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,16,4,1
-#define PRODUCTVER     2,16,4,1
-#define STRFILEVER     "2.16.4.1"
-#define STRPRODUCTVER  "2.16.4.1"
-#define STRPRIVATEBUILD "jp-1"
+#define FILEVER        2,16,4,2
+#define PRODUCTVER     2,16,4,2
+#define STRFILEVER     "2.16.4.2"
+#define STRPRODUCTVER  "2.16.4.2"
+#define STRPRIVATEBUILD "jp-2"
