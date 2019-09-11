8118a2051ed0701222adb27ee7ef387c5e8f1cda
Tim Gerundt <tim@gerundt.de>
2019-09-11 05:42:06 +0200

    Manual: Rename "WinMerge_help.xml" to "Index.xml"

--- Docs/Manual/build_htmlhelp_en.bat
+++ Docs/Manual/build_htmlhelp_en.bat
@@ -3,7 +3,7 @@
 pushd %~dp0
 call configuration.bat
 
-set docbook_inputfile=EN\WinMerge_help.xml
+set docbook_inputfile=EN\Index.xml
 set docbook_use_stylesheet=EN\xsl\htmlhelp.xsl
 set docbook_outputdir=htmlhelp
 set docbook_outputdir_final=%docbook_build_path%\%docbook_outputdir%
