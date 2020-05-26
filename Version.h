f4c06f2dafdb59f48028564135c54e885fde77d1
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2020-05-27 08:27:27 +0900

    Bump revision to 2.16.6+-jp-6

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,16,6,5
-#define PRODUCTVER     2,16,6,5
-#define STRFILEVER     "2.16.6.5"
-#define STRPRODUCTVER  "2.16.6.5"
-#define STRPRIVATEBUILD "jp-5"
+#define FILEVER        2,16,6,6
+#define PRODUCTVER     2,16,6,6
+#define STRFILEVER     "2.16.6.6"
+#define STRPRODUCTVER  "2.16.6.6"
+#define STRPRIVATEBUILD "jp-6"
