dbd0c19c37bc2f3e5352ec95d905e7fc82723a98
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2020-09-29 08:50:54 +0900

    Update PerTest.cmd

--- Testing/PerfTest.cmd
+++ Testing/PerfTest.cmd
@@ -2,9 +2,11 @@ set exepath=c:\progra~1\winmerge\winmergeu.exe
 for /F "usebackq" %%f in (`powershell -command [System.Diagnostics.FileVersionInfo]::GetVersionInfo^('%exepath%'^).FileVersion`) do (
   set resultpath=%~dp0\result-%%f-%date:/=-%%time::=.%.txt
 )
-echo [MainFrame] > "%resultpath%"
-powershell -command $^(measure-command { cmd '/c for /L %%i in (1,1,10) do "%exepath%" /noninteractive /noprefs' }^).TotalSeconds >> "%resultpath%"
-echo [MergeEditFrame] >> "%resultpath%"
-powershell -command $^(measure-command { cmd '/c for /L %%i in (1,1,10) do "%exepath%" /noninteractive /noprefs c:\progra~1\winmerge\docs\readme.txt c:\progra~1\winmerge\docs\readme.txt' }^).TotalSeconds >> "%resultpath%"
-echo [DirFrame] >> "%resultpath%"
-powershell -command $^(measure-command { cmd '/c for /L %%i in (1,1,10) do "%exepath%" /noninteractive /noprefs c:\progra~1\winmerge\docs\ c:\progra~1\winmerge\docs\' }^).TotalSeconds >> "%resultpath%"
+(
+  echo [MainFrame]
+  powershell -command $^(measure-command { cmd '/c for /L %%i in ^(1,1,10^) do "%exepath%" /noninteractive /noprefs' }^).TotalSeconds
+  echo [MergeEditFrame]
+  powershell -command $^(measure-command { cmd '/c for /L %%i in ^(1,1,10^) do "%exepath%" /noninteractive /noprefs c:\progra~1\winmerge\docs\readme.txt c:\progra~1\winmerge\docs\readme.txt' }^).TotalSeconds
+  echo [DirFrame]
+  powershell -command $^(measure-command { cmd '/c for /L %%i in ^(1,1,10^) do "%exepath%" /noninteractive /noprefs c:\progra~1\winmerge\docs\ c:\progra~1\winmerge\docs\' }^).TotalSeconds
+) > "%resultpath%"
