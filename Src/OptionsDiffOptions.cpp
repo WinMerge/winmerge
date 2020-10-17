0d8e9ae59e54e5254dce73d8e053e05e14812ba3
SamuelPlentz <SamuelPlentz@web.de>
2020-10-17 22:56:09 +0200

    CopyFullLine

--- Src/OptionsDiffOptions.cpp
+++ Src/OptionsDiffOptions.cpp
@@ -23,6 +23,7 @@ void SetDefaults(COptionsMgr *pOptionsMgr)
 	pOptionsMgr->InitOption(OPT_CMP_IGNORE_EOL, false);
 	pOptionsMgr->InitOption(OPT_CMP_DIFF_ALGORITHM, (int)0);
 	pOptionsMgr->InitOption(OPT_CMP_INDENT_HEURISTIC, true);
+	pOptionsMgr->InitOption(OPT_CMP_COPY_FULL_LINE, false);
 }
 
 void Load(const COptionsMgr *pOptionsMgr, DIFFOPTIONS& options)
@@ -34,6 +35,7 @@ void Load(const COptionsMgr *pOptionsMgr, DIFFOPTIONS& options)
 	options.bIgnoreEol = pOptionsMgr->GetBool(OPT_CMP_IGNORE_EOL);
 	options.nDiffAlgorithm = pOptionsMgr->GetInt(OPT_CMP_DIFF_ALGORITHM);
 	options.bIndentHeuristic = pOptionsMgr->GetBool(OPT_CMP_INDENT_HEURISTIC);
+	options.bCopyFullLine = pOptionsMgr->GetBool(OPT_CMP_COPY_FULL_LINE);
 }
 
 void Save(COptionsMgr *pOptionsMgr, const DIFFOPTIONS& options)
@@ -45,6 +47,7 @@ void Save(COptionsMgr *pOptionsMgr, const DIFFOPTIONS& options)
 	pOptionsMgr->SaveOption(OPT_CMP_IGNORE_EOL, options.bIgnoreEol);
 	pOptionsMgr->SaveOption(OPT_CMP_DIFF_ALGORITHM, options.nDiffAlgorithm);
 	pOptionsMgr->SaveOption(OPT_CMP_INDENT_HEURISTIC, options.bIndentHeuristic);
+	pOptionsMgr->SaveOption(OPT_CMP_COPY_FULL_LINE, options.bCopyFullLine);
 }
 
 }
