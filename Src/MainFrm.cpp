b3c83242ce6a8c548b88d2f2d7a2b32fc98ec7b5
GreyMerlin <greymerlin7@gmail.com>
2021-01-04 09:17:35 -0800

    Fix "LNT1000: A sub-expression may overflow before being assigned to a wider type" warning
    * Also fix a trivial spelling error in an unrelated comment

--- Src/MainFrm.cpp
+++ Src/MainFrm.cpp
@@ -2434,7 +2434,7 @@ void CMainFrame::OnUpdateCompareMethod(CCmdUI* pCmdUI)
 void CMainFrame::OnMRUs(UINT nID)
 {
 	std::vector<JumpList::Item> mrus = JumpList::GetRecentDocs(GetOptionsMgr()->GetInt(OPT_MRU_MAX));
-	const size_t idx = nID - ID_MRU_FIRST;
+	const size_t idx = static_cast<size_t>(nID) - ID_MRU_FIRST;
 	if (idx < mrus.size())
 	{
 		MergeCmdLineInfo cmdInfo((_T("\"") + mrus[idx].path + _T("\" ") + mrus[idx].params).c_str());
