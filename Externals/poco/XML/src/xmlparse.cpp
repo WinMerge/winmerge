1a0bc2ac2bf1e9e16e77502311d25f6a7f5611c2
GreyMerlin <greymerlin7@gmail.com>
2021-01-04 08:59:28 -0800

    Fix "C4551: function call missing argument list" warning

--- Externals/poco/XML/src/xmlparse.cpp
+++ Externals/poco/XML/src/xmlparse.cpp
@@ -6393,7 +6393,7 @@ static unsigned long FASTCALL
 hash(XML_Parser parser, KEY s) {
   struct siphash state;
   struct sipkey key;
-  (void)sip24_valid;
+  (void)sip24_valid();
   copy_salt_to_sipkey(parser, &key);
   sip24_init(&state, &key);
   sip24_update(&state, s, keylen(s) * sizeof(XML_Char));
