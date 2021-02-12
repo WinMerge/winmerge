de9e455ede76923c2e81373ac176bac909f1dbc0
Jun Tajima <56220423+tjmprm77@users.noreply.github.com>
2021-02-12 12:12:24 +0900

    Add a feature to set up default highlighting by a file extension (1)
    Merge WinMerge2011 WildcardDropList shown below.
    https://github.com/datadiode/winmerge2011/blob/ab7e14774593e170065ae1346c9f3b53ebec685b/Src/WildcardDropList.cpp
    https://github.com/datadiode/winmerge2011/blob/17a2c0e85495b4cae7658446379ff54952c48a6c/Src/WildcardDropList.h

--- Src/WildcardDropList.h
+++ Src/WildcardDropList.h
@@ -6,7 +6,9 @@ class WildcardDropList
 {
 private:
 	static LRESULT CALLBACK LbWndProc(HWND, UINT, WPARAM, LPARAM);
+	static LRESULT CALLBACK LvWndProc(HWND, UINT, WPARAM, LPARAM);
 public:
-	static void OnDropDown(HWND, int columns, LPCTSTR, bool allowUserAddedPattrens = true);
-	static void OnCloseUp(HWND);
+	static void OnDropDown(HWND, int columns, LPCTSTR, bool allowUserAddedPatterns = true);
+	static bool OnCloseUp(HWND);
+	static void OnItemActivate(HWND, int, int, int columns, LPCTSTR, bool allowUserAddedPatterns = true);
 };
