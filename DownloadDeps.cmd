fcd706dd9af71abb317594cece8d735f48437d02
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2021-04-03 23:21:21 +0900

    Update DownloadDeps.cmd

--- DownloadDeps.cmd
+++ DownloadDeps.cmd
@@ -26,4 +26,30 @@ for %%p in (%urls_destdirs%) do (
   )
 )
 
+for %%i in (Build Build\X64 Build\ARM64) do (
+  for %%j in (Release Debug) do (
+    mkdir %%i\%%j\Merge7z 2> NUL
+    mkdir %%i\%%j\WinIMerge 2> NUL
+    mkdir %%i\%%j\Frhed 2> NUL
+    mkdir %%i\%%j\Filters 2> NUL
+    mkdir %%i\%%j\ColorSchemes 2> NUL
+    mkdir %%i\%%j\MergePlugins 2> NUL
+    xcopy /s/y %%i\Merge7z %%i\%%j\Merge7z\
+    xcopy /s/y %%i\Frhed %%i\%%j\Frhed\
+    xcopy /s/y Filters %%i\%%j\Filters\
+    xcopy /s/y ColorSchemes %%i\%%j\ColorSchemes\
+    xcopy /s/y Plugins\dlls\*.sct %%i\%%j\MergePlugins\
+    if "%%i" == "Build" (
+      copy Build\WinIMerge\bin\WinIMergeLib.dll %%i\%%j\WinIMerge\
+      copy Plugins\dlls\*.dll %%i\%%j\MergePlugins\
+    ) else if "%%i" == "Build\X64" (
+      copy Build\WinIMerge\bin64\WinIMergeLib.dll %%i\%%j\WinIMerge\
+      copy Plugins\dlls\X64\*.dll %%i\%%j\MergePlugins\
+    ) else if "%%i" == "Build\ARM64" (
+      copy Build\WinIMerge\binARM64\WinIMergeLib.dll %%i\%%j\WinIMerge\
+      copy Plugins\dlls\ARM64\*.dll %%i\%%j\MergePlugins\
+    )
+  )
+)
+
 popd
