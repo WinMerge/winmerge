a7eebbcb1ef508ab1c7a969c4500b6618926437e
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2021-10-13 00:05:10 +0900

    Windows Property System (8)

--- Src/PropertySystem.cpp
+++ Src/PropertySystem.cpp
@@ -16,11 +16,30 @@
 #ifdef _WIN64
 
 #pragma comment(lib, "propsys.lib")
+ 
+ // {ECA2D096-7C87-4DFF-94E7-E7FE9BA34BE8} 100-102
+static const PROPERTYKEY PKEY_WINMERGE_HASH_SHA256 = { {0xeca2d096, 0x7c87, 0x4dff, 0x94, 0xe7, 0xe7, 0xfe, 0x9b, 0xa3, 0x4b, 0xe8}, 100 };
+static const PROPERTYKEY PKEY_WINMERGE_HASH_SHA1 ={ {0xeca2d096, 0x7c87, 0x4dff, 0x94, 0xe7, 0xe7, 0xfe, 0x9b, 0xa3, 0x4b, 0xe8}, 101 }; 
+static const PROPERTYKEY PKEY_WINMERGE_HASH_MD5 = { {0xeca2d096, 0x7c87, 0x4dff, 0x94, 0xe7, 0xe7, 0xfe, 0x9b, 0xa3, 0x4b, 0xe8}, 102 };
 
-static const PROPERTYKEY PKEY_WINMERGE_HASH_SHA256 = {};
-static const PROPERTYKEY PKEY_WINMERGE_HASH_SHA1 = {};
-static const PROPERTYKEY PKEY_WINMERGE_HASH_MD5 = {};
-static const PROPERTYKEY PKEY_WINMERGE_HASH_CRC32 = {};
+struct PROPERTYINFO
+{
+	const PROPERTYKEY* pKey;
+	const wchar_t *pszCanonicalName;
+	const wchar_t *pszDisplayName;
+};
+
+static const PROPERTYINFO g_HashProperties[] =
+{
+	{ &PKEY_WINMERGE_HASH_SHA256, L"WinMerge.Hash.SHA256", L"SHA256" },
+	{ &PKEY_WINMERGE_HASH_SHA1,   L"WinMerge.Hash.SHA1",   L"SHA1" },
+	{ &PKEY_WINMERGE_HASH_MD5,    L"WinMerge.Hash.MD5",    L"MD5" },
+};
+
+static String CalculateHashValue(const String& path, const PROPERTYKEY& key)
+{
+	return _T("TEST");
+}
 
 static bool GetPropertyString(IPropertyStore* pps, const PROPERTYKEY& key, String& value)
 {
@@ -68,7 +87,10 @@ PropertySystem::PropertySystem(ENUMFILTER filter)
 		}
 		ppdl->Release();
 	}
-	AddProperties({ _T("WinMerge.Hash.SHA1"), _T("WinMerge.Hash.SHA256"), _T("WinMerge.Hash.CRC32"), _T("WinMerge.Hash.MD5") });
+	std::vector<String> hashPropertyNames;
+	for (const auto& info : g_HashProperties)
+		hashPropertyNames.push_back(info.pszCanonicalName);
+	AddProperties(hashPropertyNames);
 }
 
 PropertySystem::PropertySystem(const std::vector<String>& canonicalNames)
@@ -83,14 +105,14 @@ void PropertySystem::AddProperties(const std::vector<String>& canonicalNames)
 		PROPERTYKEY key{};
 		if (FAILED(PSGetPropertyKeyFromName(name.c_str(), &key)))
 		{
-			if (name == _T("WinMerge.Hash.SHA256"))
-				key = PKEY_WINMERGE_HASH_SHA256;
-			else if (name == _T("WinMerge.Hash.SHA1"))
-				key = PKEY_WINMERGE_HASH_SHA1;
-			else if (name == _T("WinMerge.Hash.MD5"))
-				key = PKEY_WINMERGE_HASH_MD5;
-			else if (name == _T("WinMerge.Hash.CRC32"))
-				key = PKEY_WINMERGE_HASH_CRC32;
+			for (const auto& info : g_HashProperties)
+			{
+				if (info.pszCanonicalName == name)
+				{
+					key = *info.pKey;
+					break;
+				}
+			}
 		}
 		m_keys.push_back(key);
 		m_canonicalNames.push_back(name);
@@ -106,22 +128,19 @@ bool PropertySystem::GetFormattedValues(const String& path, std::vector<String>&
 		values.reserve(m_keys.size());
 		for (const auto& key : m_keys)
 		{
+			bool found = false;
 			String value;
-			if (!GetPropertyString(pps, key, value))
+			for (const auto& info : g_HashProperties)
 			{
-				if (key == PKEY_WINMERGE_HASH_SHA256)
+				if (*info.pKey == key)
 				{
-				}
-				else if (key == PKEY_WINMERGE_HASH_SHA1)
-				{
-				}
-				else if (key == PKEY_WINMERGE_HASH_MD5)
-				{
-				}
-				else if (key == PKEY_WINMERGE_HASH_CRC32)
-				{
+					value = CalculateHashValue(path, key);
+					found = true;
+					break;
 				}
 			}
+			if (!found)
+				GetPropertyString(pps, key, value);
 			values.push_back(value);
 		}
 		pps->Release();
@@ -159,14 +178,14 @@ bool PropertySystem::GetDisplayNames(std::vector<String>& names)
 			}
 			ppd->Release();
 		}
-		else if (key == PKEY_WINMERGE_HASH_SHA256)
-			names.push_back(_T("SHA-256"));
-		else if (key == PKEY_WINMERGE_HASH_SHA1)
-			names.push_back(_T("SHA-1"));
-		else if (key == PKEY_WINMERGE_HASH_MD5)
-			names.push_back(_T("MD5"));
-		else if (key == PKEY_WINMERGE_HASH_CRC32)
-			names.push_back(_T("CRC32"));
+		else
+		{
+			for (const auto& info : g_HashProperties)
+			{
+				if (*info.pKey == key)
+					names.push_back(info.pszDisplayName);
+			}
+		}
 	}
 	return true;
 }
