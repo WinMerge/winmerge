362c97707a0721cf2f1f98beafc4cea857872c4b
stonee-K <105475799+stonee-K@users.noreply.github.com>
2022-05-20 08:23:45 +0900

    Fix typo: GPL
    Fix typo: "GNU DIFF General Public License" should be "GNU General Public License".

--- Src/diffutils/src/side.c
+++ Src/diffutils/src/side.c
@@ -7,12 +7,12 @@ GNU DIFF is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY.  No author or distributor
 accepts responsibility to anyone for the consequences of using it
 or for whether it serves any particular purpose or works at all,
-unless he says so in writing.  Refer to the GNU DIFF General Public
+unless he says so in writing.  Refer to the GNU General Public
 License for full details.
 
 Everyone is granted permission to copy, modify and redistribute
 GNU DIFF, but only under the conditions described in the
-GNU DIFF General Public License.   A copy of this license is
+GNU General Public License.   A copy of this license is
 supposed to have been given to you along with GNU DIFF so you
 can know your rights and responsibilities.  It should be in a
 file named COPYING.  Among other things, the copyright notice
