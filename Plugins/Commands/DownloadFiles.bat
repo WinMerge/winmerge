700f06835d9100c7bd6ee480c1f89781bf2dc309
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2023-01-22 10:54:40 +0900

    Add Plugins/Commands/DownloadFiles.bat (2)

--- Plugins/Commands/DownloadFiles.bat
+++ Plugins/Commands/DownloadFiles.bat
@@ -2,7 +2,7 @@
 for %%i in (Apache-Tika Java PlantUML q yq) do (
   for /F %%j in ('type %%i\URL.txt') do (
     echo Downloading %%j
-    mkdir "%APPDATA%\WinMerge\Command\%%i" 2> NUL
-    powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri %%j -UseBasicParsing -Outfile '%APPDATA%\WinMerge\Command\%%i\%%~nxj'"
+    mkdir "%APPDATA%\WinMerge\Commands\%%i" 2> NUL
+    powershell -command "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Invoke-WebRequest -Uri %%j -UseBasicParsing -Outfile '%APPDATA%\WinMerge\Commands\%%i\%%~nxj'"
   )
 )
