c9c3d2100b29eaaca89d91b02d972c43a7f208ab
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2023-02-11 09:47:21 +0900

    Update winwebdiff to 1.0.6

--- DownloadDeps.cmd
+++ DownloadDeps.cmd
@@ -24,9 +24,9 @@ https://github.com/WinMerge/winimerge/releases/download/v1.0.37/winimerge-1.0.37
 https://github.com/WinMerge/winimerge/releases/download/v1.0.37/winimerge-1.0.37.0-x64.zip!Build\x64\Release ^
 https://github.com/WinMerge/winimerge/releases/download/v1.0.37/winimerge-1.0.37.0-ARM.zip!Build\ARM\Release ^
 https://github.com/WinMerge/winimerge/releases/download/v1.0.37/winimerge-1.0.37.0-ARM64.zip!Build\ARM64\Release ^
-https://github.com/WinMerge/winwebdiff/releases/download/v1.0.5/winwebdiff-1.0.5.0-x86.zip!Build\x86\Release ^
-https://github.com/WinMerge/winwebdiff/releases/download/v1.0.5/winwebdiff-1.0.5.0-x64.zip!Build\x64\Release ^
-https://github.com/WinMerge/winwebdiff/releases/download/v1.0.5/winwebdiff-1.0.5.0-ARM64.zip!Build\ARM64\Release ^
+https://github.com/WinMerge/winwebdiff/releases/download/v1.0.6/winwebdiff-1.0.6.0-x86.zip!Build\x86\Release ^
+https://github.com/WinMerge/winwebdiff/releases/download/v1.0.6/winwebdiff-1.0.6.0-x64.zip!Build\x64\Release ^
+https://github.com/WinMerge/winwebdiff/releases/download/v1.0.6/winwebdiff-1.0.6.0-ARM64.zip!Build\ARM64\Release ^
 https://github.com/WinMerge/patch/releases/download/v2.5.9-7/patch-2.5.9-7-bin.zip!Build\GnuWin32 ^
 https://github.com/htacg/tidy-html5/releases/download/5.4.0/tidy-5.4.0-w32-mt-XP.zip!Build\tidy-html5 ^
 https://github.com/htacg/tidy-html5/archive/refs/tags/5.4.0.zip!Build\tidy-html5 ^
