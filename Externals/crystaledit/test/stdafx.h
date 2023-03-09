78022c49cc2fa6c9a14bc4efa64cc556db1175f0
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2023-03-09 23:19:21 +0900

    Add undoRecordTests.cpp

--- Externals/crystaledit/test/stdafx.h
+++ Externals/crystaledit/test/stdafx.h
@@ -11,6 +11,6 @@
 #include "CppUnitTest.h"
 
 // TODO: reference additional headers your program requires here
-#include <Windows.h>
-#include <tchar.h>
+#define _AFXDLL
+#include <afxwin.h>         // MFC core and standard components
 #define ASSERT(x)
