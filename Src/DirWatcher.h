00f0c572fc57b99115fc55e5429a4829ece706dd
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2023-03-17 09:17:10 +0900

    refactor

--- Src/DirWatcher.h
+++ Src/DirWatcher.h
@@ -6,12 +6,7 @@
 #pragma once
 
 #include "UnicodeString.h"
-#include <map>
-#include <vector>
 #include <functional>
-#include <Windows.h>
-
-struct DirRequest;
 
 class DirWatcher
 {
@@ -19,21 +14,11 @@ public:
 	enum ACTION { ACTION_ADDED = 1, ACTION_REMOVED, ACTION_MODIFIED, ACTION_RENAMED_OLD_NAME, ACTION_RENAMED_NEW_NAME };
 	DirWatcher();
 	~DirWatcher();
-	DirWatcher(const DirWatcher &) = delete;
-	DirWatcher & operator=(const DirWatcher &) = delete;
 	bool Add(uintptr_t id, bool dir, const String& path, std::function<void(const String&, ACTION)> callback);
 	bool Remove(uintptr_t id);
 	void Clear();
 private:
-	bool startThread();
-	bool exitThread();
-	unsigned DirWatcherThreadProc();
-	static unsigned __stdcall DirWatcherThreadProcStatic(void* pParam);
-	HANDLE m_hThread;
-	HANDLE m_hEventReq;
-	HANDLE m_hEventResp;
-	HRESULT m_resp;
-	std::unique_ptr<DirRequest> m_pReq;
-	CRITICAL_SECTION m_cs;
+	class Impl;
+	std::unique_ptr<Impl> m_pimpl;
 };
 
