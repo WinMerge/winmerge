6cf5a451566d770ea817d45d5601e66cfe0b4bcd
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2023-08-06 17:01:06 +0900

    Make it work in environments like Windows Sandbox where Short Name Creations is disabled

--- Testing/PerfTest.cmd
+++ Testing/PerfTest.cmd
@@ -1,15 +1,17 @@
 if "%1" == "" (
-  set exepath=c:\progra~1\winmerge\winmergeu.exe
+  set exepath=c:\program files\winmerge\winmergeu.exe
 ) else (
   set exepath=%1
 )
+for %%i in ("%exepath%") do set exedir=%%~dpi
+
 if not exist %~dp0result.csv (
   echo Date,Version,MainFrame,MergeEditFrame,DirFrame,Report > "%~dp0result.csv"
 )
 for /F "usebackq" %%f in (`powershell -command [System.Diagnostics.FileVersionInfo]::GetVersionInfo^('%exepath%'^).FileVersion`) do set Version=%%f
-for /F "usebackq" %%f in (`powershell -command $^(measure-command { cmd '/c for /L %%i in ^(1,1,10^) do "%exepath%" /noninteractive /noprefs' }^).TotalSeconds`) do set MainFrame=%%f
-for /F "usebackq" %%f in (`powershell -command $^(measure-command { cmd '/c for /L %%i in ^(1,1,10^) do "%exepath%" /noninteractive /noprefs c:\progra~1\winmerge\docs\readme.txt c:\progra~1\winmerge\docs\readme.txt' }^).TotalSeconds`) do set MergeEditFrame=%%f
-for /F "usebackq" %%f in (`powershell -command $^(measure-command { cmd '/c for /L %%i in ^(1,1,10^) do "%exepath%" /noninteractive /noprefs c:\progra~1\winmerge\docs\ c:\progra~1\winmerge\docs\' }^).TotalSeconds`) do set DirFrame=%%f
+for /F "usebackq" %%f in (`powershell -command $^(measure-command { for ^($i ^= 0^; $i -lt 10^; $i++^) { ^^^& '%exepath%' /noninteractive /noprefs ^^^| Out-Null } }^).TotalSeconds`) do set MainFrame=%%f
+for /F "usebackq" %%f in (`powershell -command $^(measure-command { for ^($i ^= 0^; $i -lt 10^; $i++^) { ^^^& '%exepath%' /noninteractive /noprefs '%exedir%docs\readme.txt' '%exedir%docs\readme.txt' ^^^| Out-Null } }^).TotalSeconds`) do set MergeEditFrame=%%f
+for /F "usebackq" %%f in (`powershell -command $^(measure-command { for ^($i ^= 0^; $i -lt 10^; $i++^) { ^^^& '%exepath%' /noninteractive /noprefs '%exedir%docs\' '%exedir%docs\' ^^^| Out-Null } }^).TotalSeconds`) do set DirFrame=%%f
 if "%Version:2.16=%" == "%Version%" goto :SKIP_REPORT
 for /F "usebackq" %%f in (`powershell -command $^(measure-command { cmd '/c %~dp0\ReportTest.cmd' }^).TotalSeconds`) do set Report=%%f
 :SKIP_REPORT
