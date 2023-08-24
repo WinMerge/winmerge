e019a6a24272dabe629fd52549ac6cae1c7330cb
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2023-08-25 07:51:31 +0900

    Use po4a for manual translation (3)

--- Docs/Manual/UpdateTranslations.bat
+++ Docs/Manual/UpdateTranslations.bat
@@ -1,4 +1,5 @@
-cd %~dp0
+@echo off
+pushd %~dp0
 if "%MSYS_HOME%" == "" (
   for %%i in (C D E F G) do (
     if exist %%i:\msys64\ set MSYS_HOME=%%i:\msys64
@@ -12,4 +13,4 @@ echo po4a --verbose ./Manual.cfg
 ) > %MSYS_HOME%\tmp\__update_translations__.sh
 %MSYS_HOME%\usr\bin\bash.exe -lc /tmp/__update_translations__.sh
 del %MSYS_HOME%\tmp\__update_translations__.sh
-pause
+popd
