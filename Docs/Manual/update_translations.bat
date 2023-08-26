af8faf46522b1efe65ae076646170b1a3b034a08
sdottaka <sdottaka@users.sourceforge.net>
2023-08-26 21:21:29 +0900

    Manual: Use po4a for manual translation (#1994) (3)

--- Docs/Manual/update_translations.bat
+++ Docs/Manual/update_translations.bat
@@ -9,7 +9,7 @@ if "%MSYS_HOME%" == "" (
 (
 echo #!/bin/bash
 echo cd $^(cygpath '%CD%'^)
-echo po4a --verbose --package-name WinMerge --package-version 2.16 --copyright-holder 'Thingamahoochie Software' --msgid-bugs-address https://bugs.winmerge.org/ ./Manual.cfg
+echo po4a --verbose --wrap-po=no --package-name WinMerge --package-version 2.16 --copyright-holder 'Thingamahoochie Software' --msgid-bugs-address https://bugs.winmerge.org/ ./Manual.cfg
 ) > %MSYS_HOME%\tmp\__update_translations__.sh
 %MSYS_HOME%\usr\bin\bash.exe -lc /tmp/__update_translations__.sh
 del %MSYS_HOME%\tmp\__update_translations__.sh
