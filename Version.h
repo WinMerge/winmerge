6fdf032d70e86ec916ea58ac5baec02c21b27992
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2023-12-24 08:50:10 +0900

    Bump revision to 2.16.37

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,16,36,0
-#define PRODUCTVER     2,16,36,0
-#define STRFILEVER     "2.16.36.0"
-#define STRPRODUCTVER  "2.16.36.0"
+#define FILEVER        2,16,37,0
+#define PRODUCTVER     2,16,37,0
+#define STRFILEVER     "2.16.37.0"
+#define STRPRODUCTVER  "2.16.37.0"
 #define STRPRIVATEBUILD ""
