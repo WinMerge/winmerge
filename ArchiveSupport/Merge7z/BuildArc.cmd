d0a24201aefd746b012127adc021703ce7dfc6df
sdottaka <sdottaka@users.sourceforge.net>
2024-10-11 09:00:21 +0900

    Update 7-Zip to 24.08

--- ArchiveSupport/Merge7z/BuildArc.cmd
+++ ArchiveSupport/Merge7z/BuildArc.cmd
@@ -1,7 +1,7 @@
 pushd "%~dp0"
 
 setlocal
-set MERGE7z_VERSION=2407.1
+set MERGE7z_VERSION=2408.0
 set PATH="%ProgramFiles%\7-zip";"%ProgramFiles(x86)%\7-zip";%PATH%
 set DISTDIR=..\..\Build\Releases
 
