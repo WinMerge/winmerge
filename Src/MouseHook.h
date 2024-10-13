ed2e423eec6701db26cdc5eedcbabac48ee0ec36
sdottaka <sdottaka@users.sourceforge.net>
2024-10-13 16:50:17 +0900

    Add One-Hand mice wheel scroll diff and merge (#2435) (11)

--- Src/MouseHook.h
+++ Src/MouseHook.h
@@ -3,7 +3,7 @@ class CMouseHook
 public:
 	static void SetMouseHook();
 	static void UnhookMouseHook();
-	static bool IsRightWheelScrolling() { return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - m_endTimeRightWheelScrolling).count() < 100; }
+	static bool IsRightWheelScrolling() { return m_bIgnoreRBUp || (std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now() - m_endTimeRightWheelScrolling).count() < 100); }
 private:
 	static LRESULT CALLBACK MouseProc(int nCode, WPARAM wParam, LPARAM lParam);
 	inline static void StartRightWheelScrolling() { m_bIgnoreRBUp = true; }
