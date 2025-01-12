e3cd3b4d1b8bb7941398648340468daaa9bab5a6
sdottaka <sdottaka@users.sourceforge.net>
2025-01-13 08:24:42 +0900

    Bump revision to 2.16.46

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,16,45,1
-#define PRODUCTVER     2,16,45,1
-#define STRFILEVER     "2.16.45.1"
-#define STRPRODUCTVER  "2.16.45.1"
+#define FILEVER        2,16,46,0
+#define PRODUCTVER     2,16,46,0
+#define STRFILEVER     "2.16.46.0"
+#define STRPRODUCTVER  "2.16.46.0"
 #define STRPRIVATEBUILD ""
