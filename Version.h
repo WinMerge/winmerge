1dacea66fec1e98f78818a5a80d71bda773874e9
sdottaka <sdottaka@users.sourceforge.net>
2025-04-29 20:03:21 +0900

    Bump revision to 2.16.48.2
    (cherry picked from commit 453c0768fda89048c56ce061f8bcdefa9cc90168)

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,16,48,0
-#define PRODUCTVER     2,16,48,0
-#define STRFILEVER     "2.16.48.0"
-#define STRPRODUCTVER  "2.16.48.0"
+#define FILEVER        2,16,48,2
+#define PRODUCTVER     2,16,48,2
+#define STRFILEVER     "2.16.48.2"
+#define STRPRODUCTVER  "2.16.48.2"
 #define STRPRIVATEBUILD ""
