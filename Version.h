0c1f561d8cd170ad31d5ccd401479ef13d1085c6
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2025-10-23 21:26:50 +0900

    Bump revison to 2.16.52

--- Version.h
+++ Version.h
@@ -1,5 +1,5 @@
-#define FILEVER        2,16,51,0
-#define PRODUCTVER     2,16,51,0
-#define STRFILEVER     "2.16.51.0"
-#define STRPRODUCTVER  "2.16.51.0"
+#define FILEVER        2,16,52,0
+#define PRODUCTVER     2,16,52,0
+#define STRFILEVER     "2.16.52.0"
+#define STRPRODUCTVER  "2.16.52.0"
 #define STRPRIVATEBUILD ""
