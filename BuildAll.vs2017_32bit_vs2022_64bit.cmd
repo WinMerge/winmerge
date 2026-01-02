e5fad27bcbd77ee78d2396a0b7ad7de1eb71cb1d
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2026-01-02 19:37:44 +0900

    Revert documentation changes to continue Windows 7 support

---
+++ BuildAll.vs2017_32bit_vs2022_64bit.cmd
@@ -0,0 +1,20 @@
+pushd "%~dp0"
+call BuildManual.cmd || goto :eof
+setlocal
+call BuildBin.vs2017.cmd x86 || goto :eof
+endlocal
+setlocal
+call BuildBin.vs2017.cmd ARM || goto :eof
+endlocal
+setlocal
+call BuildBin.vs2022.cmd ARM64 || goto :eof
+endlocal
+setlocal
+call BuildBin.vs2022.cmd x64 || goto :eof
+endlocal
+pushd Testing\GoogleTest\UnitTests
+UnitTests.exe || goto :eof
+popd
+call BuildInstaller.cmd %1 %2
+call BuildArc.cmd %1 %2
+popd
