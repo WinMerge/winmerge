ec9aacd48ffd7fd3287fa882e6864ca02890f682
Tuomas Hietanen <thorium@iki.fi>
2026-06-22 12:32:11 +0100

    A thread-safety issue fixed: TreeSitterRegistry::Initialize/GetLanguageForName mutates shared maps with no lock, called concurrently from folder-compare workers. (#3427)

--- Src/TreeSitterParser.cpp
+++ Src/TreeSitterParser.cpp
@@ -1413,6 +1413,8 @@ TreeSitterRegistry& TreeSitterRegistry::Instance()
 
 void TreeSitterRegistry::Initialize(const std::wstring& sGrammarDir)
 {
+	std::lock_guard<std::mutex> lock(m_mutex);
+
 	if (m_bInitialized)
 		return;
 
@@ -1473,6 +1475,8 @@ void TreeSitterRegistry::Initialize(const std::wstring& sGrammarDir)
 
 const CTreeSitterLanguage* TreeSitterRegistry::GetLanguageForName(const std::wstring& sLangName)
 {
+	std::lock_guard<std::mutex> lock(m_mutex);
+
 	// Check if already loaded
 	auto itLang = m_languages.find(sLangName);
 	if (itLang != m_languages.end())
