a0f84c160c8c5b4c9b230273af37369292b6244c
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2026-08-22 19:47:03 +0900

    Preserve file encoding when applying prediffers

--- Testing/GoogleTest/multiformatText/multiformatText_test.cpp
+++ Testing/GoogleTest/multiformatText/multiformatText_test.cpp
@@ -88,8 +88,8 @@ namespace
 			// detect Ansi or Unicode file
 			bufferData.SetDataFileUnknown(_T("../../Data/Unicode/UTF-8/DiffItem.h"), false);
 			String filename;
-			BSTR* buf = bufferData.GetDataBufferUnicode();
-			bufferData.SaveAsFile(filename);
+			ASSERT_NE(nullptr, bufferData.GetDataBufferUnicode());
+			EXPECT_TRUE(bufferData.SaveAsFile(filename));
 			auto encoding = codepage_detect::Guess(filename, 0);
 			EXPECT_EQ(ucr::UNICODESET::UTF8, encoding.m_unicoding);
 			}
@@ -99,7 +99,7 @@ namespace
 			bufferData.SetDataFileUnknown(_T("../../Data/Unicode/UCS-2LE/DiffItem.h"), false);
 			String filename;
 			filename = bufferData.GetDataFileUnicode();
-			bufferData.SaveAsFile(filename);
+			EXPECT_TRUE(bufferData.SaveAsFile(filename));
 			auto encoding = codepage_detect::Guess(filename, 0);
 			EXPECT_EQ(ucr::UNICODESET::UCS2LE, encoding.m_unicoding);
 		}
@@ -108,8 +108,8 @@ namespace
 			storageForPlugins bufferData;
 			bufferData.SetDataFileUnknown(_T("../../Data/Unicode/UCS-2BE/DiffItem.h"), false);
 			String filename;
-			BSTR* buf = bufferData.GetDataBufferUnicode();
-			bufferData.SaveAsFile(filename);
+			ASSERT_NE(nullptr, bufferData.GetDataBufferUnicode());
+			EXPECT_TRUE(bufferData.SaveAsFile(filename));
 			auto encoding = codepage_detect::Guess(filename, 0);
 			EXPECT_EQ(ucr::UNICODESET::UCS2BE, encoding.m_unicoding);
 		}
