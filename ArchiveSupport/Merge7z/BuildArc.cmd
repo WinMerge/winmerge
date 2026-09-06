5040542c6db007275421e2745a97decc73ff8bb9
Takashi Sawanaka <sdottaka@users.sourceforge.net>
2026-09-06 21:18:50 +0900

    Update 7-Zip to 26.03

--- ArchiveSupport/Merge7z/BuildArc.cmd
+++ ArchiveSupport/Merge7z/BuildArc.cmd
@@ -1,7 +1,7 @@
 pushd "%~dp0"
 
 setlocal
-set MERGE7z_VERSION=2602.1
+set MERGE7z_VERSION=2603.0
 set PATH="%ProgramFiles%\7-zip";"%ProgramFiles(x86)%\7-zip";%PATH%
 set DISTDIR=..\..\Build\Releases
 
@@ -32,9 +32,9 @@ if "%PLATFORM%" == "x86" (
 set ZIP_FILENAME=Merge7z%MERGE7Z_VERSION%-%PLATFORM%.zip
 
 if "%PLATFORM%" == "ARM64" (
-  copy /y "..\..\Externals\sevenzip\CPP\7zip\Bundles\Format7zF\arm64\7z.dll" %DISTDIR%\%PLATFORM%\Merge7z\
+  copy /y "..\..\Externals\7zip\CPP\7zip\Bundles\Format7zF\arm64\7z.dll" %DISTDIR%\%PLATFORM%\Merge7z\
 ) else if "%PLATFORM%" == "ARM" (
-  copy /y "..\..\Externals\sevenzip\CPP\7zip\Bundles\Format7zF\arm\7z.dll" %DISTDIR%\%PLATFORM%\Merge7z\
+  copy /y "..\..\Externals\7zip\CPP\7zip\Bundles\Format7zF\arm\7z.dll" %DISTDIR%\%PLATFORM%\Merge7z\
 ) else (
   copy /y "%ProgramFilesP%\7-Zip\7z.dll" %DISTDIR%\%PLATFORM%\Merge7z\
 )
